5ed7a45c5d61810866fcb0f78c5d4ba022b42025
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-02-03 21:01:38 +0100

    Update HEADER and module headers

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# {{cookiecutter.description}} 
-# Copyright (C) {% now 'local', '%Y' %} {{cookiecutter.author}} <{{cookiecutter.email}}>
+# GBasis: Python library for Gaussian basis function evaluation & integrals.
 #
-# This file is part of {{cookiecutter.project_name}}.
+# Copyright (C) 2019 HORTON-ChemTools Dev Team <horton.chemtools@gmail.com>.
 #
-# {{cookiecutter.project_name}} is free software; you can redistribute it and/or
+# This file is part of GBasis.
+#
+# GBasis is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# {{cookiecutter.project_name}} is distributed in the hope that it will be useful,
+# GBasis is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>
+#
 # --
 """GBasis Setup Script.
 
