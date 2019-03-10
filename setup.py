e11db93d51695bae5d2bc7106da566683233beb3
Taewon D. Kim <kimt33@mcmaster.ca>
2019-03-10 18:40:26 -0400

    Apply black to setup.py

--- setup.py
+++ setup.py
@@ -13,27 +13,24 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='gbasis',
-    version='0.0.0',
-    description='A module for evaluating, differentiating, and integrating Gaussian functions.',
+    name="gbasis",
+    version="0.0.0",
+    description="A module for evaluating, differentiating, and integrating Gaussian functions.",
     long_description=long_description,
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
-    long_description_content_type='text/markdown',
-    url='https://github.com/pypa/sampleproject',
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
     # This should be your name or the name of the organization which owns the
     # project.
     # author='',
-
     # This should be a valid email address corresponding to the author listed
     # above.
     # author_email='',
-
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -43,37 +40,39 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 1 - Planning',
-
+        "Development Status :: 1 - Planning",
         # Indicate who your project is intended for
-        'Intended Audience :: Quantum Chemists',
-        'Topic :: Gaussian :: Integral :: Differentiation :: Evaluation',
-
+        "Intended Audience :: Quantum Chemists",
+        "Topic :: Gaussian :: Integral :: Differentiation :: Evaluation",
         # Pick your license as you wish
-        'License :: OSI Approved :: GNU Version 3',
-
+        "License :: OSI Approved :: GNU Version 3",
         # Specify the Python versions you support here.
         # These classifiers are *not* checked by 'pip install'. See instead
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-
-    keywords='gaussian integration differentiation evaluation density',
-    packages=find_packages(exclude=['docs', 'tests']),
-    python_requires='>=3.5',
-    install_requires=['numpy', 'scipy'],
-
+    keywords="gaussian integration differentiation evaluation density",
+    packages=find_packages(exclude=["docs", "tests"]),
+    python_requires=">=3.5",
+    install_requires=["numpy", "scipy"],
     extras_require={
-        'dev': ['tox', 'flake8', 'flake8-pydocstyle', 'flake8-import-order',
-                'pep8-naming', 'pylint', 'bandit', 'pytest', 'pytest-cov'],
-        'test': ['tox', 'pytest', 'pytest-cov'],
+        "dev": [
+            "tox",
+            "flake8",
+            "flake8-pydocstyle",
+            "flake8-import-order",
+            "pep8-naming",
+            "pylint",
+            "bandit",
+            "pytest",
+            "pytest-cov",
+        ],
+        "test": ["tox", "pytest", "pytest-cov"],
     },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     package_data={},
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -86,11 +85,10 @@ setup(
     #         'sample=sample:main',
     #     ],
     # },
-
     # List additional URLs that are relevant to your project as a dict.
     project_urls={
-        'Bug Reports': 'https://github.com/theochem/gbasis/issues',
-        'Organization': 'https://github.com/quantumelephant/',
-        'Source': 'https://github.com/theochem/gbasis/',
+        "Bug Reports": "https://github.com/theochem/gbasis/issues",
+        "Organization": "https://github.com/quantumelephant/",
+        "Source": "https://github.com/theochem/gbasis/",
     },
 )
