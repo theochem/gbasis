b1c0863f95ca9a666b19e7e9742d432ec8e564f7
William Adams <wil.h.adams@gmail.com>
2019-03-21 13:38:10 -0400

    Add function for evaluating contracted spherical Gaussians

--- gbasis/spherical.py
+++ gbasis/spherical.py
@@ -1,4 +1,6 @@
 """Convert from Cartesian Gaussians to Spherical Gaussians."""
+from gbasis.deriv import eval_shell
+from gbasis.lincomb import lincomb_blocks_evals
 import numpy as np
 from scipy.special import comb, factorial
 
@@ -228,9 +230,11 @@ def generate_transformation(angmom, cartesian_order, apply_from):
     ----------
     angmom : int
         The angular momentum of the basis function(s).
-    cartesian_order : list(len = 2 * angmom + 1) of tuple(len = 3)
-        The order of the Cartesian primitives, with each tuple holding
-        components in the order a_x, a_y, a_z.
+    cartesian_order : np.ndarray((angmom + 1) * (angmom + 2) / 2, 3)
+        The x, y, and z components of the angular momentum for each primitive,
+        (:math:`\vec{a} = (a_x, a_y, a_z)`, where :math:`a_x + a_y + a_z = l`).
+        The order in which the primitives are given defines the order in which
+        they will appear in the transformation matrix.
     apply_from : str
         The side on which the transformation matrix is applied. One of {"left", "right"}.
 
@@ -256,17 +260,13 @@ def generate_transformation(angmom, cartesian_order, apply_from):
         raise TypeError("Angular momentum must be an integer.")
     if angmom < 0:
         raise ValueError("Angular momentum must be a non-negative integer.")
-    if not isinstance(cartesian_order, list):
-        raise TypeError("The order of the Cartesian primitives must be a list.")
-    if not all(isinstance(t, tuple) for t in cartesian_order):
-        raise TypeError("The Cartesian primitives' components must be a tuple.")
-    if not all(len(t) == 3 for t in cartesian_order):
-        raise ValueError("Each Cartesian primitive must have exactly three components.")
-    if len(cartesian_order) < ((angmom + 1) * (angmom + 2) / 2):
-        raise ValueError("Too few Cartesian primitives given.")
-    if len(cartesian_order) > ((angmom + 1) * (angmom + 2) / 2):
-        raise ValueError("Too many Cartesian primitives given.")
-    if not all(np.sum(t) == angmom for t in cartesian_order):
+    if not isinstance(cartesian_order, np.ndarray):
+        raise TypeError("The order of the Cartesian primitives must be an array.")
+    if not cartesian_order.shape == ((angmom + 1)*(angmom + 2) / 2, 3):
+        raise ValueError(
+            "The order of the Cartesian primitives must have dimensions {}".format((angmom, 3))
+        )
+    if not np.all(np.sum(cartesian_order, axis=1) == angmom):
         raise ValueError("Each primitive's components must sum to the angular momentum.")
     if not isinstance(apply_from, str):
         raise TypeError("Specify the side on which the transformation is applied as a string")
@@ -286,3 +286,42 @@ def generate_transformation(angmom, cartesian_order, apply_from):
     if apply_from == "left":
         return transform.T
     return transform
+
+
+def eval_spherical_shell(*, coords, shell):
+    """Return the set of spherical contractions evaluated at the given coordinates.
+
+    Parameters
+    ----------
+    coords : np.ndarray(N, 3)
+        Point in space where the Gaussian primitive is evaluated.
+    shell : ContractedCartesianGaussians
+        Set of contracted Cartesian Gaussians with the same angular momentum.
+
+    Returns
+    -------
+    derivative : np.ndarray(L, N)
+        Evaluation of the (zeroth-order) derivative.
+        :math:`L` is the number of contractions associated with the given `shell`.
+
+    Raises
+    ------
+    TypeError
+        If the arguments are given as positional arguments.
+        If coords is not a numpy array.
+        If shell is not a ContractedCartesianGaussians.
+    ValueError
+        If coords is not a two-dimensional numpy array with 3 columns.
+
+    Notes
+    -----
+    When calling this function, the arguments must be given via keywords and not positional
+    arguments. This feature is used to catch problems that arise due to a change in API.
+
+    """
+    return lincomb_blocks_evals(
+        [shell],
+        eval_shell,
+        [generate_transformation(shell.angmom, shell.angmom_components, apply_from="left")],
+        coords=coords,
+    )
