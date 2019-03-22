54bb7c101c6e3b428b5e8c4d2f0705bd5372d053
Taewon D. Kim <kimt33@mcmaster.ca>
2019-03-21 22:16:47 -0400

    Removed split of derivatized and non-derivatized coordinates
    Got slower

--- gbasis/deriv.py
+++ gbasis/deriv.py
@@ -61,87 +61,49 @@ def _eval_deriv_contractions(coords, orders, center, angmom_comps, alphas, prim_
     # NOTE: if `angmom_comps` is two-dimensional (3, L), has shape (1, 1, 3, L).
     alphas = alphas[np.newaxis, :, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
     # NOTE: `prim_coeffs` will be used as a 1D array
+    orders = orders[np.newaxis, np.newaxis, :, :, np.newaxis, np.newaxis]
 
     # useful variables
     rel_coords = coords - center
     gauss = np.exp(-alphas * rel_coords ** 2)
 
-    # zeroth order (i.e. no derivatization)
-    bool_noderiv = (orders <= 0)[np.newaxis, np.newaxis, :, :, np.newaxis, np.newaxis]
-    zero_rel_coords, zero_angmom_comps, zero_gauss = (
-        rel_coords * bool_noderiv,
-        angmom_comps * bool_noderiv,
-        gauss * bool_noderiv,
+    # General approach: compute the whole coefficents, zero out the irrelevant parts
+    # NOTE: The following step assumes that there is only one set (nx, ny, nz) of derivatization
+    # orders i.e. we assume that only one axis (axis 2) of `nonzero_orders` has a dimension
+    # greater than 1
+    indices_herm = np.arange(np.max(orders) + 1, dtype=int)[:, None, None, None, None, None]
+    # get indices that are used as powers of the appropriate terms in the derivative
+    # NOTE: the negative indices must be turned into zeros (even though they are turned into
+    # zeros later anyways) because these terms are sometimes zeros (and negative power is
+    # undefined).
+    indices_angmom = angmom_comps - orders + indices_herm
+    indices_angmom[indices_angmom < 0] = 0
+    # get coefficients for all entries
+    coeffs = (
+        comb(orders, indices_herm)
+        * perm(angmom_comps, orders - indices_herm)
+        * (-alphas ** 0.5) ** indices_herm
+        * rel_coords ** indices_angmom
     )
-    zeroth_part = np.zeros(
-        (alphas.shape[1], bool_noderiv.shape[2], angmom_comps.shape[4], rel_coords.shape[5])
-    )
-    for i in range(bool_noderiv.shape[2]):
-        zeroth_part[:, i] = np.prod(
-            zero_rel_coords[:, :, i, bool_noderiv[0, 0, i, :, 0, 0], :, :]
-            ** zero_angmom_comps[:, :, i, bool_noderiv[0, 0, i, :, 0, 0], :, :]
-            * zero_gauss[:, :, i, bool_noderiv[0, 0, i, :, 0, 0], :, :],
-            axis=(0, 2),
-        )
-    # NOTE: `zeroth_part` now has axis 0 for primitives, axis 1 for order of derivatization, and
-    # axis 1 for angular momentum vector, and axis 2 for coordinate
+    # zero out the appropriate terms
+    indices_zero = np.where(indices_herm < np.maximum(0, orders - angmom_comps))
+    coeffs[indices_zero[0], :, indices_zero[2], indices_zero[3], indices_zero[4], :] = 0
+    indices_zero = np.where(orders < indices_herm)
+    coeffs[indices_zero[0], :, indices_zero[2], indices_zero[3], :, :] = 0
+    # compute
+    # TODO: I don't know if the scipy.special.eval_hermite uses some smart vectorizing/caching
+    # to evaluate multiple orders at the same time. Creating/finding a better function for
+    # evaluating the hermite polynomial at different orders (in sequence) may be nice in the
+    # future.
+    hermite = np.sum(coeffs * eval_hermite(indices_herm, alphas ** 0.5 * rel_coords), axis=0)
+    hermite = np.prod(hermite, axis=2)
+    # NOTE: `hermite` now has axis 0 for primitives, 1 for order of derivatization, 2 for
+    # angular momentum vector, axis 3 for coordinates
 
-    deriv_part = 1
-    bool_deriv = (orders > 0)[np.newaxis, np.newaxis, :, :, np.newaxis, np.newaxis]
-    indices_deriv = np.where(bool_deriv)
-    nonzero_rel_coords, nonzero_orders, nonzero_angmom_comps, nonzero_gauss = (
-        rel_coords * bool_deriv,
-        orders[np.newaxis, np.newaxis, :, :, np.newaxis, np.newaxis] * bool_deriv,
-        angmom_comps * bool_deriv,
-        gauss * bool_deriv,
-    )
-
-    # derivatization part
-    if indices_deriv[0].size != 0:
-        # General approach: compute the whole coefficents, zero out the irrelevant parts
-        # NOTE: The following step assumes that there is only one set (nx, ny, nz) of derivatization
-        # orders i.e. we assume that only one axis (axis 2) of `nonzero_orders` has a dimension
-        # greater than 1
-        indices_herm = np.arange(np.max(nonzero_orders) + 1)[:, None, None, None, None, None]
-        # get indices that are used as powers of the appropriate terms in the derivative
-        # NOTE: the negative indices must be turned into zeros (even though they are turned into
-        # zeros later anyways) because these terms are sometimes zeros (and negative power is
-        # undefined).
-        indices_angmom = nonzero_angmom_comps - nonzero_orders + indices_herm
-        indices_angmom[indices_angmom < 0] = 0
-        # get coefficients for all entries
-        coeffs = (
-            comb(nonzero_orders, indices_herm)
-            * perm(nonzero_angmom_comps, nonzero_orders - indices_herm)
-            * (-alphas ** 0.5) ** indices_herm
-            * nonzero_rel_coords ** indices_angmom
-        )
-        # zero out the appropriate terms
-        indices_zero = np.where(indices_herm < np.maximum(0, nonzero_orders - nonzero_angmom_comps))
-        coeffs[indices_zero[0], :, indices_zero[2], indices_zero[3], indices_zero[4], :] = 0
-        indices_zero = np.where(nonzero_orders < indices_herm)
-        coeffs[indices_zero[0], :, indices_zero[2], indices_zero[3], :, :] = 0
-        # compute
-        # TODO: I don't know if the scipy.special.eval_hermite uses some smart vectorizing/caching
-        # to evaluate multiple orders at the same time. Creating/finding a better function for
-        # evaluating the hermite polynomial at different orders (in sequence) may be nice in the
-        # future.
-        hermite = np.sum(
-            coeffs * eval_hermite(indices_herm, alphas ** 0.5 * nonzero_rel_coords), axis=0
-        )
-        hermite = np.prod(hermite, axis=2)
-
-        # NOTE: `hermite` now has axis 0 for primitives, 1 for order of derivatization, 2 for
-        # angular momentum vector, axis 3 for coordinates
-        nonzero_gauss_2 = np.zeros(hermite.shape)
-        for i in range(nonzero_orders.shape[2]):
-            nonzero_gauss_2[:, i] = np.prod(
-                nonzero_gauss[:, :, i, bool_deriv[0, 0, i, :, 0, 0], :, :], axis=(0, 2)
-            )
-        deriv_part = nonzero_gauss_2 * hermite
+    deriv_part = np.prod(gauss, axis=(0, 3)) * hermite
 
     norm = norm.T[:, np.newaxis, :, np.newaxis]
-    return np.tensordot(prim_coeffs, norm * zeroth_part * deriv_part, (0, 0))
+    return np.tensordot(prim_coeffs, norm * deriv_part, (0, 0))
 
 
 def eval_deriv_shell(*, coords, orders, shell):
