bd579f434d0b05e2ebcf20bdac7b04d0b77b4085
Taewon D. Kim <kimt33@mcmaster.ca>
2019-03-23 13:01:04 -0400

    Add base class for arrays with two indices related to one set of contractions
    What:
    1. Add BaseTwoIndexSymmetric abstract class
    2. Add __init__ and ignore pylint's complaints
    3. Add properties `contractions_one` and `contractions_two`
    4. Add abstract method `construct_array_contraction`
    5. Add methods `construct_array_cartesian`, `construct_array_spherical`, and
    `construct_array_spherical_lincomb`.
    5. Add tests
    
    Why:
    1. This class will be used to create overlap, moment, and other one-electron
    integrals.
    2. Despite pylint's complaints, this __init__ forces the number of arguments to
    be two, rather than an arbitrary number of its parrent.
    3. Since there is only two set of contractions within an instance of this class,
    this property will make things a little more readable (`self.contractions_one` vs
    `self._axes_contractions[0]`)
    4. Though this function still doesn't do anything (just like its parent's), this
    method is created again for documentation purposes
    5. These methods are common throughout all one-index objects

---
+++ gbasis/base_two_symm.py
@@ -0,0 +1,230 @@
+"""Base class for arrays that depend on two contracted Gaussians."""
+import abc
+
+from gbasis.base import BaseGaussianRelatedArray
+from gbasis.spherical import generate_transformation
+import numpy as np
+
+
+# pylint: disable=W0235
+class BaseTwoIndexSymmetric(BaseGaussianRelatedArray):
+    """Base class for constructing arrays associated with two contracted Gaussian.
+
+    The first two axes of the returned array is associated with the given set of contracted Gaussian
+    (or a linear combination of a set of Gaussians).
+
+    Attributes
+    ----------
+    _axes_contractions : tuple of tuple of ContractedCartesianGaussians
+        Sets of contractions associated with each axis of the array.
+
+    Properties
+    ----------
+    contractions : tuple of ContractedCartesianGaussians
+        Contractions that are associated with the first and second indices of the array.
+
+    Methods
+    -------
+    __init__(self, contractions)
+        Initialize.
+    construct_array_contraction(self, contraction) : np.ndarray
+        Return the array associated with a `ContractedCartesianGaussians` instance.
+    construct_array_cartesian(self) : np.ndarray
+        Return the array associated with Cartesian Gaussians.
+    construct_array_spherical(self) : np.ndarray
+        Return the array associated with spherical Gaussians (atomic orbitals).
+    construct_array_spherical_lincomb(self, transform) : np.ndarray
+        Return the array associated with linear combinations of spherical Gaussians (linear
+        combinations of atomic orbitals).
+
+    """
+
+    def __init__(self, contractions):
+        """Initialize.
+
+        Parameters
+        ----------
+        contractions : list/tuple of ContractedCartesianGaussians
+            Contractions that are associated with the first and second indices of the array.
+
+        """
+        super().__init__(contractions)
+
+    @property
+    def contractions(self):
+        """Contractions that are associated with the first index of the array.
+
+        Returns
+        -------
+        contractions : tuple of ContractedCartesianGaussians
+            Contractions that are associated with the first index of the array.
+
+        """
+        return self._axes_contractions[0]
+
+    # NOTE: check if output is symmetric when contractions_one = contractions_two?
+    @abc.abstractmethod
+    def construct_array_contraction(self, contractions_one, contractions_two, **kwargs):
+        """Return the array associated with a set of contracted Cartesian Gaussians.
+
+        Parameters
+        ----------
+        contractions_one : ContractedCartesianGaussians
+            Contracted Cartesian Gaussians (of the same shell) associated with the first index of
+            the array.
+        contractions_two : ContractedCartesianGaussians
+            Contracted Cartesian Gaussians (of the same shell) associated with the second index of
+            the array.
+        kwargs : dict
+            Other keyword arguments that will be used to construct the array.
+
+        Returns
+        -------
+        array_contraction : np.ndarray
+            Array associated with the given instance(s) of ContractedCartesianGaussians.
+            First axis corresponds to the first set of contractions.
+            Second axis corresponds to the second set of contrations.
+            This array should be symmetric with respect to the swapping of the first and second
+            axes.
+
+        Notes
+        -----
+        The next level of classes will be the construction of specific arrays given a set of
+        contracted Cartesian Gaussians. The keyword arguments will be different depending on its
+        functionality. You should use explicit keyword arguments when defining this function, rather
+        than the arbitrary number of keywords (as is done here).
+
+        """
+
+    def construct_array_cartesian(self, **kwargs):
+        """Return the array associated with the given set(s) of contracted Cartesian Gaussians.
+
+        Parameters
+        ----------
+        kwargs : dict
+            Other keyword arguments that will be used to construct the array.
+            These keyword arguments are passed entirely to `construct_array_contrations`. See
+            `construct_array_contractions` for details on the keyword arguments.
+
+        Returns
+        -------
+        array : np.ndarray
+            Array associated with the given set of contracted Cartesian Gaussians.
+            First and second indices of the array is associated with the contracted Cartesian
+            Gaussians.
+
+        Notes
+        -----
+        For the blocks along the diagonal i.e. blocks where the first two axis belong to the same
+        set of contractions, they are transposed in the processed of constructing the whole array.
+        It is assumed that the array returned from `construct_array_contraction` is symmetric with
+        respect to the swapping of the first and second axes.
+
+        """
+        triu_blocks = [
+            self.construct_array_contraction(cont_one, cont_two, **kwargs)
+            for i, cont_one in enumerate(self.contractions)
+            for cont_two in self.contractions[i:]
+        ]
+        # use numpy triu and tril indices to create blocks
+        num_blocks_side = len(self.contractions)
+        all_blocks = np.zeros((num_blocks_side, num_blocks_side), dtype=object)
+        all_blocks[np.triu_indices(num_blocks_side)] = triu_blocks
+        all_blocks[np.tril_indices(num_blocks_side)] = [
+            np.swapaxes(block, 0, 1) for block in triu_blocks
+        ]
+        # concatenate
+        return np.concatenate(
+            [np.concatenate(row_blocks, axis=1) for row_blocks in all_blocks], axis=0
+        )
+
+    def construct_array_spherical(self, **kwargs):
+        """Return the array associated with two contracted spherical Gaussians (atomic orbitals).
+
+        Parameters
+        ----------
+        kwargs : dict
+            Other keyword arguments that will be used to construct the array.
+            These keyword arguments are passed entirely to `construct_array_contrations`. See
+            `construct_array_contractions` for details on the keyword arguments.
+
+        Returns
+        -------
+        array : np.ndarray
+            Array associated with the atomic orbitals associated with the given set(s) of contracted
+            Cartesian Gaussians.
+            First and second indices of the array is associated with two contracted spherical
+            Gaussians (atomic orbitals).
+
+        Notes
+        -----
+        For the blocks along the diagonal i.e. blocks where the first two axis belong to the same
+        set of contractions, they are transposed in the processed of constructing the whole array.
+        It is assumed that the array returned from `construct_array_contraction` is symmetric with
+        respect to the swapping of the first and second axes.
+
+        """
+        triu_blocks = []
+        for i, cont_one in enumerate(self.contractions):
+            # get transformation from cartesian to spherical (applied to left)
+            transform_one = generate_transformation(
+                cont_one.angmom, cont_one.angmom_components, "left"
+            )
+            for cont_two in self.contractions[i:]:
+                transform_two = generate_transformation(
+                    cont_two.angmom, cont_two.angmom_components, "left"
+                )
+                # evaluate
+                block_sph = self.construct_array_contraction(cont_one, cont_two, **kwargs)
+                # transform
+                block_sph = np.tensordot(transform_one, block_sph, (1, 0))
+                block_sph = np.tensordot(transform_two, block_sph, (1, 1))
+                block_sph = np.swapaxes(block_sph, 0, 1)
+                # store
+                triu_blocks.append(block_sph)
+        # use numpy triu and tril indices to create blocks
+        num_blocks_side = len(self.contractions)
+        all_blocks = np.zeros((num_blocks_side, num_blocks_side), dtype=object)
+        all_blocks[np.triu_indices(num_blocks_side)] = triu_blocks
+        all_blocks[np.tril_indices(num_blocks_side)] = [
+            np.swapaxes(block, 0, 1) for block in triu_blocks
+        ]
+        # concatenate
+        return np.concatenate(
+            [np.concatenate(row_blocks, axis=1) for row_blocks in all_blocks], axis=0
+        )
+
+    def construct_array_spherical_lincomb(self, transform, **kwargs):
+        r"""Return the array associated with linear combinations of spherical Gaussians (LCAO's).
+
+        .. math::
+
+            \sum_{j} T_{i_1 j} \sum_{k} T_{i_2 k} M_{jklm...} = M^{trans}_{i_1 i_2 lm...}
+
+        Parameters
+        ----------
+        transform : np.ndarray
+            Array associated with the linear combinations of spherical Gaussians (LCAO's) associated
+            with the first and second index.
+        kwargs : dict
+            Other keyword arguments that will be used to construct the array.
+            These keyword arguments are passed directly to `construct_array_spherical`, which will
+            then pass it down to `construct_array_contrations`. See `construct_array_contractions`
+            for details on the keyword arguments.
+
+        Returns
+        -------
+        array : np.ndarray
+            Array whose first and second indices associated with the linear combinations of the
+            contracted spherical Gaussians.
+            First index of the array corresponds to the linear combination of contracted spherical
+            Gaussians associated with the first set of contractions, `contractions_one`.
+            Second index of the array corresponds to the linear combination of contracted spherical
+            Gaussians associated with the second set of contractions, `contractions_two`.
+
+        """
+        array_transformed = self.construct_array_spherical(**kwargs)
+        array_transformed = np.tensordot(transform, array_transformed, (1, 0))
+        array_transformed = np.tensordot(transform, array_transformed, (1, 1))
+        array_transformed = np.swapaxes(array_transformed, 0, 1)
+        return array_transformed
