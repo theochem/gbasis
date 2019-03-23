9d712e0e5511e881a600aeb0f7a980ff862b47c9
Taewon D. Kim <kimt33@mcmaster.ca>
2019-03-23 04:23:15 -0400

    Ignore pylint warning for changing number of arguments when overriding methods
    It is difficult to keep the number of arguments the same because the methods
    `construct_array_contractions` and `construct_array_spherical_lincomb` will have
    different number of arugments depending on the number of indices that are
    associated with Gaussian contractions (or related objects). Therefore, the
    warning is removed.

--- gbasis/base.py
+++ gbasis/base.py
@@ -82,6 +82,16 @@ class BaseGaussianRelatedArray(abc.ABC):
         array_contraction : np.ndarray
             Array associated with the given instance(s) of ContractedCartesianGaussians.
 
+        Notes
+        -----
+        The next level of classes will be divided by number of indices associated with
+        ContractedCartesianGaussians. Then this method's parameters will likely be different from
+        it's children. This means that using this method's API blindly (by simply copying and
+        pasting) will not be suitable for all its children. Here, we allow arbitrary number of
+        `contractions` to indicate that its children may have different number of `contractions` in
+        its parameters. However, in actual implementations, the number of `contractions` will likely
+        be fixed, in which case, an arbitrary number of `contractions` should not be accepted.
+
         """
 
     @abc.abstractmethod
@@ -135,5 +145,15 @@ class BaseGaussianRelatedArray(abc.ABC):
         array : np.ndarray
             Array associated with the atomic orbitals associated with the given set of contracted
             Cartesian Gaussians.
+
+        Notes
+        -----
+        The next level of classes will be divided by number of indices associated with
+        ContractedCartesianGaussians. Then this method's parameters will likely be different from
+        it's children. This means that using this method's API blindly (by simply copying and
+        pasting) will not be suitable for all its children. Here, we allow arbitrary number of
+        `transform` to indicate that its children may have different number of `transform` in
+        its parameters. However, in actual implementations, the number of `transform` will likely
+        be fixed, in which case, an arbitrary number of `transform` should not be accepted.
 
         """
