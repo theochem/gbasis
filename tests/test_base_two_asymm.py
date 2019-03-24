3d005c906201203150b6c30aa5944e3264f4a099
Taewon D. Kim <kimt33@mcmaster.ca>
2019-03-24 13:46:26 -0400

    Add base class for arrays with two indices related to two sets of contractions
    What:
    1. Add BaseTwoIndexAsymmetric abstract class
    2. Add __init__ and ignore pylint's complaints
    3. Add properties `contractions`
    4. Add abstract method `construct_array_contraction`
    5. Add methods `construct_array_cartesian`, `construct_array_spherical`, and
    `construct_array_spherical_lincomb`.
    5. Add tests
    
    Why:
    1. This class will be used to create overlap, moment, and other one-electron
    integrals.
    2. Despite pylint's complaints, this __init__ forces the number of arguments to
    be two, rather than an arbitrary number of its parrent.
    3. Since there is only two set of contractions within an instance of this class,
    this property will make things a little more readable (`self.contractions` vs
    `self._axes_contractions[0]`)
    4. Though this function still doesn't do anything (just like its parent's), this
    method is created again for documentation purposes
    5. These methods are common throughout all one-index objects

---
+++ tests/test_base_two_asymm.py
@@ -0,0 +1,165 @@
+"""Test gbasis.base_two_asymm."""
+from gbasis.base_two_asymm import BaseTwoIndexAsymmetric
+from gbasis.contractions import ContractedCartesianGaussians
+from gbasis.spherical import generate_transformation
+import numpy as np
+import pytest
+from utils import disable_abstract, skip_init
+
+
+def test_init():
+    """Test BaseTwoIndexAsymmetric.__init__."""
+    Test = disable_abstract(BaseTwoIndexAsymmetric)  # noqa: N806
+    test = skip_init(Test)
+    contractions = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    Test.__init__(test, [contractions], [contractions])
+    assert test._axes_contractions == ((contractions,), (contractions,))
+    with pytest.raises(TypeError):
+        Test.__init__(test, [contractions])
+    with pytest.raises(TypeError):
+        Test.__init__(test, [contractions], [contractions], [contractions])
+
+
+def test_contractions_one():
+    """Test BaseTwoIndexAsymmetric.constractions_one."""
+    Test = disable_abstract(BaseTwoIndexAsymmetric)  # noqa: N806
+    cont_one = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    cont_two = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    test = Test([cont_one], [cont_two])
+    assert test.contractions_one[0] == cont_one
+
+
+def test_contractions_two():
+    """Test BaseTwoIndexAsymmetric.constractions_two."""
+    Test = disable_abstract(BaseTwoIndexAsymmetric)  # noqa: N806
+    cont_one = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    cont_two = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    test = Test([cont_one], [cont_two])
+    assert test.contractions_two[0] == cont_two
+
+
+def test_contruct_array_contraction():
+    """Test BaseTwoIndexAsymmetric.construct_array_contraction."""
+    # enable only the abstract method construct_array_contraction
+    Test = disable_abstract(  # noqa: N806
+        BaseTwoIndexAsymmetric,
+        dict_overwrite={
+            "construct_array_contraction": BaseTwoIndexAsymmetric.construct_array_contraction
+        },
+    )
+    contractions = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    with pytest.raises(TypeError):
+        Test([contractions])
+
+
+def test_contruct_array_cartesian():
+    """Test BaseTwoIndexAsymmetric.construct_array_cartesian."""
+    contractions = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    Test = disable_abstract(  # noqa: N806
+        BaseTwoIndexAsymmetric,
+        dict_overwrite={
+            "construct_array_contraction": lambda self, cont1, cont2, a=2: np.ones((2, 2)) * a
+        },
+    )
+    test = Test([contractions], [contractions])
+    assert np.allclose(test.construct_array_cartesian(), np.ones((2, 2)) * 2)
+    assert np.allclose(test.construct_array_cartesian(a=3), np.ones((2, 2)) * 3)
+    with pytest.raises(TypeError):
+        test.construct_array_cartesian(bad_keyword=3)
+
+    test = Test([contractions, contractions], [contractions])
+    assert np.allclose(test.construct_array_cartesian(), np.ones((4, 2)) * 2)
+    assert np.allclose(test.construct_array_cartesian(a=3), np.ones((4, 2)) * 3)
+
+    Test = disable_abstract(  # noqa: N806
+        BaseTwoIndexAsymmetric,
+        dict_overwrite={
+            "construct_array_contraction": lambda self, cont_one, cont_two, a=2: np.ones((2, 5)) * a
+        },
+    )
+    test = Test([contractions, contractions], [contractions])
+    assert np.allclose(test.construct_array_cartesian(), np.ones((4, 5)) * 2)
+    assert np.allclose(test.construct_array_cartesian(a=3), np.ones((4, 5)) * 3)
+
+
+def test_contruct_array_spherical():
+    """Test BaseTwoIndexAsymmetric.construct_array_spherical."""
+    contractions = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    transform = generate_transformation(1, contractions.angmom_components, "left")
+
+    Test = disable_abstract(  # noqa: N806
+        BaseTwoIndexAsymmetric,
+        dict_overwrite={
+            "construct_array_contraction": (
+                lambda self, cont_one, cont_two, a=2: np.arange(9).reshape(3, 3) * a
+            )
+        },
+    )
+    test = Test([contractions], [contractions])
+    assert np.allclose(
+        test.construct_array_spherical(),
+        transform.dot(np.arange(9).reshape(3, 3)).dot(transform.T) * 2,
+    )
+    assert np.allclose(
+        test.construct_array_spherical(a=3),
+        transform.dot(np.arange(9).reshape(3, 3)).dot(transform.T) * 3,
+    )
+    with pytest.raises(TypeError):
+        test.construct_array_spherical(bad_keyword=3)
+
+    test = Test([contractions, contractions], [contractions])
+    assert np.allclose(
+        test.construct_array_spherical(),
+        np.vstack([transform.dot(np.arange(9).reshape(3, 3).dot(transform.T)) * 2] * 2),
+    )
+
+
+def test_contruct_array_spherical_lincomb():
+    """Test BaseTwoIndexAsymmetric.construct_array_spherical_lincomb."""
+    contractions = ContractedCartesianGaussians(1, np.array([1, 2, 3]), 0, np.ones(1), np.ones(1))
+    sph_transform = generate_transformation(1, contractions.angmom_components, "left")
+    orb_transform_one = np.random.rand(3, 3)
+    orb_transform_two = np.random.rand(3, 3)
+
+    Test = disable_abstract(  # noqa: N806
+        BaseTwoIndexAsymmetric,
+        dict_overwrite={
+            "construct_array_contraction": (
+                lambda self, cont_one, cont_two, a=2: np.arange(9).reshape(3, 3) * a
+            )
+        },
+    )
+    test = Test([contractions], [contractions])
+    assert np.allclose(
+        test.construct_array_spherical_lincomb(orb_transform_one, orb_transform_two),
+        (
+            orb_transform_one.dot(sph_transform)
+            .dot(np.arange(9).reshape(3, 3))
+            .dot(sph_transform.T)
+            .dot(orb_transform_two.T)
+            * 2
+        ),
+    )
+    assert np.allclose(
+        test.construct_array_spherical_lincomb(orb_transform_one, orb_transform_two, a=3),
+        (
+            orb_transform_one.dot(sph_transform)
+            .dot(np.arange(9).reshape(3, 3))
+            .dot(sph_transform.T)
+            .dot(orb_transform_two.T)
+            * 3
+        ),
+    )
+    with pytest.raises(TypeError):
+        test.construct_array_spherical_lincomb(bad_keyword=3)
+
+    orb_transform_one = np.random.rand(3, 6)
+    test = Test([contractions, contractions], [contractions])
+    assert np.allclose(
+        test.construct_array_spherical_lincomb(orb_transform_one, orb_transform_two),
+        orb_transform_one.dot(
+            np.vstack(
+                [sph_transform.dot(np.arange(9).reshape(3, 3)).dot(sph_transform.T) * 2] * 2
+            ).dot(orb_transform_two.T)
+        ),
+    )
