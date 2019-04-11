28d54459d7c027681825235bd3b60c0e7ea5d0cc
Taewon D. Kim <kimt33@mcmaster.ca>
2019-04-11 14:07:04 -0400

    Polish up density evaluation code
    1. Multidimensional array support can be dropped since the lower end code no
    longer supports it
    2. Change name of function. The name is little vague at the moment
    3. Minor changes in docstrings and type checks

--- gbasis/density.py
+++ gbasis/density.py
@@ -2,56 +2,57 @@
 import numpy as np
 
 
-def eval_density(density_mat, orb_eval):
-    """Return the evaluation of the density at different points.
+def eval_density_using_evaluated_orbs(one_density_matrix, orb_eval):
+    """Return the evaluation of the density given the evaluated orbitals.
 
     Parameters
     ----------
-    density_mat : np.ndarray(L, L)
-        One electron density matrix.
-    orb_eval : {np.ndarray(L), np.ndarray(L, N), np.ndarray(L, N, N, N)}
-        Array of orbitals evaluated at different grid points.
-        The set of orbitals must be the same basis set used to build the charge density matrix.
+    one_density_matrix : np.ndarray(K_orb, K_orb)
+        One-electron density matrix.
+    orb_eval : np.ndarray(K_orb, N)
+        Orbitals evaluated at different grid points.
+        The set of orbitals must be the same basis set used to build the one-electron density
+        matrix.
 
     Returns
     -------
-    total_density : {np.ndarray(N, 1), np.ndarray(N, N, N)}
+    density : np.ndarray(N,)
         Density evaluated at different grid points.
-        Dimension of the density will match the dimension the grid part of orb_eval.
 
     Raises
     ------
     TypeError
-        If orb_eval is not an array of floats.
-        If density_mat is not an array of floats.
+        If orb_eval is not a 2-dimensional numpy array with dtype float.
+        If density_matrix is not a 2-dimensional numpy array with dtype float.
     ValueError
-        If density_mat is not a 2-dimensional matrix.
-        If first dimension of density_mat and orb_eval are not equal.
+        If one-electron density matrix is not square.
+        If the number of columns (or rows) of the one-electron density matrix is not equal to the
+        number of rows of the orbital evaluations.
 
     """
     # test that inputs have the correct shape and type
-    if not (isinstance(orb_eval, np.ndarray) and orb_eval.dtype == float):
-        raise TypeError("Evaluation of orbitals must be a numpy array of data type float.")
-    if not (isinstance(density_mat, np.ndarray) and density_mat.dtype == float):
-        raise TypeError("Density matrix must be a numpy array of data type float.")
-    if density_mat.ndim != 2 or density_mat.shape[0] != density_mat.shape[1]:
-        raise ValueError("Density matrix must be a square matrix.")
-    if density_mat.shape[0] != orb_eval.shape[0]:
-        raise ValueError("Size of 1st dimension of the density matrix and orbitals must match.")
+    if not (
+        isinstance(one_density_matrix, np.ndarray)
+        and one_density_matrix.ndim == 2
+        and one_density_matrix.dtype == float
+    ):
+        raise TypeError(
+            "One-electron density matrix must be a two-dimensional numpy array with dtype float."
+        )
+    if not (isinstance(orb_eval, np.ndarray) and orb_eval.ndim == 2 and orb_eval.dtype == float):
+        raise TypeError(
+            "Evaluation of orbitals must be a two-dimensional numpy array with dtype float."
+        )
+    if one_density_matrix.shape[0] != one_density_matrix.shape[1]:
+        raise ValueError("One-electron density matrix must be a square matrix.")
+    if not np.allclose(one_density_matrix, one_density_matrix.T):
+        raise ValueError("One-electron density matrix must be symmetric.")
+    if one_density_matrix.shape[0] != orb_eval.shape[0]:
+        raise ValueError(
+            "Number of rows (and columns) of the density matrix must be equal to the number of rows"
+            " of the orbital evaluations."
+        )
 
-    # resize orbital array to 2 dimensions
-    old_shape = orb_eval.shape
-    orb_eval.shape = (orb_eval.shape[0], np.prod(old_shape[1:]))
-
-    # evaluate partial density contributions
-    partial_density = (
-        orb_eval[np.newaxis, :] * orb_eval[:, np.newaxis] * density_mat[:, :, np.newaxis]
-    )
-
-    # sum over all orbital pairs
-    total_density = partial_density.sum(axis=(0, 1))
-
-    # resize density array
-    total_density.shape = old_shape[1:]
-
-    return total_density
+    density = one_density_matrix.dot(orb_eval)
+    density *= orb_eval
+    return np.sum(density, axis=0)
