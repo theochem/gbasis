35b7c6d0d669b09d8f77e2aaefd26728b01f0096
Taewon D. Kim <kimt33@mcmaster.ca>
2019-04-11 14:33:59 -0400

    Add function for evaluating the density from basis set
    This function is porcelain that will probably be used to evaluate the density of
    molecular orbitals (when the basis set, transformation matrix, and the
    coordinates for evaluation are available)

--- tests/test_density.py
+++ tests/test_density.py
@@ -1,7 +1,11 @@
 """Test gbasis.density."""
-from gbasis.density import eval_density_using_evaluated_orbs
+from gbasis.contractions import make_contractions
+from gbasis.density import eval_density_using_basis, eval_density_using_evaluated_orbs
+from gbasis.eval import evaluate_basis_spherical_lincomb
+from gbasis.parsers import parse_nwchem
 import numpy as np
 import pytest
+from utils import find_datafile
 
 
 def test_eval_density_using_evaluated_orbs():
@@ -45,3 +49,21 @@ def test_eval_density_using_evaluated_orbs():
     with pytest.raises(ValueError):
         density_mat = np.array([[1.0, 2.0], [3.0, 4.0]])
         eval_density_using_evaluated_orbs(density_mat, orb_eval)
+
+
+def test_eval_density_using_basis():
+    """Test gbasis.density.eval_density_using_basis."""
+    with open(find_datafile("data_sto6g.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+    basis = make_contractions(basis_dict, ["Kr"], np.array([[0, 0, 0]]))
+    transform = np.random.rand(14, 18)
+    density = np.random.rand(14, 14)
+    density += density.T
+    coords = np.random.rand(10, 3)
+
+    eval_orbs = evaluate_basis_spherical_lincomb(basis, coords, transform)
+    assert np.allclose(
+        eval_density_using_basis(density, basis, coords, transform),
+        np.einsum("ij,ik,jk->k", density, eval_orbs, eval_orbs),
+    )
