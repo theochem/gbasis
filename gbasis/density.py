ef3b0b930c96de8f15c39f480febfcdfa12809f3
Taewon D. Kim <kimt33@mcmaster.ca>
2019-04-11 16:07:45 -0400

    Add function for evaluating arbitrary order derivative of density

--- gbasis/density.py
+++ gbasis/density.py
@@ -1,6 +1,8 @@
 """Density Evaluation."""
 from gbasis.eval import evaluate_basis_spherical_lincomb
+from gbasis.eval_deriv import evaluate_deriv_basis_spherical_lincomb
 import numpy as np
+from scipy.special import comb
 
 
 def eval_density_using_evaluated_orbs(one_density_matrix, orb_eval):
@@ -88,3 +90,58 @@ def eval_density_using_basis(one_density_matrix, basis, coords, transform):
     """
     orb_eval = evaluate_basis_spherical_lincomb(basis, coords, transform)
     return eval_density_using_evaluated_orbs(one_density_matrix, orb_eval)
+
+
+def eval_deriv_density_using_basis(orders, one_density_matrix, basis, coords, transform):
+    """Return the derivative of density of the given transformed basis set at the given coordinates.
+
+    Parameters
+    ----------
+    orders : np.ndarray(3,)
+        Orders of the derivative.
+    one_density_matrix : np.ndarray(K_orb, K_orb)
+        One-electron density matrix.
+    basis : list/tuple of ContractedCartesianGaussians
+        Contracted Cartesian Gaussians (of the same shell) that will be used to construct an array.
+    coords : np.ndarray(N, 3)
+        Points in space where the contractions are evaluated.
+    transform : np.ndarray(K_orbs, K_sph)
+        Array associated with the linear combinations of spherical Gaussians (LCAO's).
+        Transformation is applied to the left, i.e. the sum is over the second index of `transform`
+        and first index of the array for contracted spherical Gaussians.
+
+    Returns
+    -------
+    density_deriv : np.ndarray(N,)
+        Derivative of the density evaluated at different grid points.
+
+    """
+    # pylint: disable=R0914
+    total_l_x, total_l_y, total_l_z = orders
+
+    output = np.zeros(coords.shape[0])
+    for l_x in range(total_l_x // 2 + 1):
+        # prevent double counting for the middle of the even total_l_x
+        # e.g. If total_l_x == 4, then l_x is in [0, 1, 2, 3, 4]. Exploiting symmetry we only need
+        # to loop over [0, 1, 2] because l_x in [0, 4] and l_x in [1, 3] give the same result.
+        # However, l_x = 2 needs to avoid double counting.
+        if total_l_x % 2 == 0 and l_x == total_l_x / 2:
+            factor = 1
+        else:
+            factor = 2
+        for l_y in range(total_l_y + 1):
+            for l_z in range(total_l_z + 1):
+                num_occurence = comb(total_l_x, l_x) * comb(total_l_y, l_y) * comb(total_l_z, l_z)
+                orders_one = np.array([l_x, l_y, l_z])
+                orders_two = orders - orders_one
+                deriv_orb_eval_one = evaluate_deriv_basis_spherical_lincomb(
+                    basis, coords, orders_one, transform
+                )
+                deriv_orb_eval_two = evaluate_deriv_basis_spherical_lincomb(
+                    basis, coords, orders_two, transform
+                )
+                density = one_density_matrix.dot(deriv_orb_eval_two)
+                density *= deriv_orb_eval_one
+                density = np.sum(density, axis=0)
+                output += factor * num_occurence * density
+    return output
