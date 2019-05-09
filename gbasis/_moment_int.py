bc174b929c39a22adeb5936851dde5a2a823e236
William Adams <wil.h.adams@gmail.com>
2019-05-09 17:28:59 -0400

    Rename alphas to exps to correspond with usage in gbasis.contractions

--- gbasis/_moment_int.py
+++ gbasis/_moment_int.py
@@ -10,12 +10,12 @@ def _compute_multipole_moment_integrals(
     orders_moment,
     coord_a,
     angmoms_a,
-    alphas_a,
+    exps_a,
     coeffs_a,
     norm_a,
     coord_b,
     angmoms_b,
-    alphas_b,
+    exps_b,
     coeffs_b,
     norm_b,
 ):
@@ -35,7 +35,7 @@ def _compute_multipole_moment_integrals(
         Angular momentum vectors (lx, ly, lz) for the contractions on the left side.
         Note that a two dimensional array must be given, even if there is only one angular momentum
         vector.
-    alphas_a : np.ndarray(K_a,)
+    exps_a : np.ndarray(K_a,)
         Values of the (square root of the) precisions of the primitives on the left side.
     coeffs_a : np.ndarray(K_a, M_a)
         Contraction coefficients of the primitives on the left side.
@@ -50,7 +50,7 @@ def _compute_multipole_moment_integrals(
         Angular momentum vectors (lx, ly, lz) for the contractions on the right side.
         Note that a two dimensional array must be given, even if there is only one angular momentum
         vector.
-    alphas_b : np.ndarray(K_b,)
+    exps_b : np.ndarray(K_b,)
         Values of the (square root of the) precisions of the primitives on the right side.
     coeffs_b : np.ndarray(K_b, M_b)
         Contraction coefficients of the primitives on the right side.
@@ -94,31 +94,31 @@ def _compute_multipole_moment_integrals(
     order_moment_max = np.max(orders_moment)
 
     integrals = np.zeros(
-        (order_moment_max + 1, angmom_b_max + 1, angmom_a_max + 1, 3, alphas_b.size, alphas_a.size)
+        (order_moment_max + 1, angmom_b_max + 1, angmom_a_max + 1, 3, exps_b.size, exps_a.size)
     )
 
     # adjust axis
     coord_moment = coord_moment[np.newaxis, np.newaxis, np.newaxis, :, np.newaxis, np.newaxis]
     coord_a = coord_a[np.newaxis, np.newaxis, np.newaxis, :, np.newaxis, np.newaxis]
     coord_b = coord_b[np.newaxis, np.newaxis, np.newaxis, :, np.newaxis, np.newaxis]
-    alphas_a = alphas_a[np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis, :]
-    alphas_b = alphas_b[np.newaxis, np.newaxis, np.newaxis, np.newaxis, :, np.newaxis]
+    exps_a = exps_a[np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis, :]
+    exps_b = exps_b[np.newaxis, np.newaxis, np.newaxis, np.newaxis, :, np.newaxis]
     # NOTE: coeffs_a and coeffs_b are not flattened because tensordot will be used at the end where
     # the primitives are transformed to contractions
 
     # sum of the exponents
-    alphas_sum = alphas_a + alphas_b
+    exps_sum = exps_a + exps_b
     # coordinate of the weighted average center
-    coord_wac = (alphas_a * coord_a + alphas_b * coord_b) / alphas_sum
+    coord_wac = (exps_a * coord_a + exps_b * coord_b) / exps_sum
     # relative distance from weighted average center
     rel_coord_a = coord_wac - coord_a
     rel_coord_b = coord_wac - coord_b
     rel_coord_moment = coord_wac - coord_moment
     # harmonic mean
-    harm_mean = alphas_a * alphas_b / alphas_sum
+    harm_mean = exps_a * exps_b / exps_sum
 
     # start of recursion
-    integrals[0, 0, 0, :, :, :] = np.sqrt(np.pi / alphas_sum) * np.exp(
+    integrals[0, 0, 0, :, :, :] = np.sqrt(np.pi / exps_sum) * np.exp(
         -harm_mean * (coord_a - coord_b) ** 2
     )
 
@@ -127,7 +127,7 @@ def _compute_multipole_moment_integrals(
     integrals[0, 0, 1:2, :, :, :] = rel_coord_a * integrals[0, 0, 0:1, :, :, :]
     for i in range(1, angmom_a_max):
         integrals[0, 0, i + 1, :, :, :] = rel_coord_a * integrals[0, 0, i, :, :, :] + (
-            i * integrals[0, 0, i - 1, :, :, :] / (2 * alphas_sum)
+            i * integrals[0, 0, i - 1, :, :, :] / (2 * exps_sum)
         )
 
     # recurse over angular momentum for b
@@ -135,47 +135,47 @@ def _compute_multipole_moment_integrals(
     # NOTE: array is sliced to avoid an if statement for angmom_b_max > 0
     integrals[0, 1:2, 0, :, :, :] = rel_coord_b * integrals[0, 0:1, 0, :, :, :]
     integrals[0, 1:2, 1:, :, :, :] = rel_coord_b * integrals[0, 0:1, 1:, :, :, :] + (
-        i_range[0, 0:1, 1:, :, :, :] * integrals[0, 0:1, :-1, :, :, :] / (2 * alphas_sum)
+        i_range[0, 0:1, 1:, :, :, :] * integrals[0, 0:1, :-1, :, :, :] / (2 * exps_sum)
     )
     for j in range(1, angmom_b_max):
         integrals[0, j + 1, 0, :, :, :] = rel_coord_b * integrals[0, j, 0, :, :, :] + (
-            j * integrals[0, j - 1, 0, :, :, :] / (2 * alphas_sum)
+            j * integrals[0, j - 1, 0, :, :, :] / (2 * exps_sum)
         )
         integrals[0, j + 1, 1:, :, :, :] = rel_coord_b * integrals[0, j, 1:, :, :, :] + (
             i_range[0, 0, 1:, :, :, :] * integrals[0, j, :-1, :, :, :]
             + j * integrals[0, j - 1, 1:, :, :, :]
-        ) / (2 * alphas_sum)
+        ) / (2 * exps_sum)
     # recurse over order of moment
     j_range = np.arange(angmom_b_max + 1)[None, :, None, None, None, None]
     # NOTE: array is sliced to avoid an if statement for angmom_b_max > 0
     integrals[1:2, 0, 0, :, :, :] = rel_coord_moment * integrals[0:1, 0, 0, :, :, :]
     integrals[1:2, 0, 1:, :, :, :] = rel_coord_moment * integrals[0:1, 0, 1:, :, :, :] + (
-        i_range[0:1, 0, 1:, :, :, :] * integrals[0:1, 0, :-1, :, :, :] / (2 * alphas_sum)
+        i_range[0:1, 0, 1:, :, :, :] * integrals[0:1, 0, :-1, :, :, :] / (2 * exps_sum)
     )
     integrals[1:2, 1:, 0, :, :, :] = rel_coord_moment * integrals[0:1, 1:, 0, :, :, :] + (
-        j_range[0:1, 1:, 0, :, :, :] * integrals[0:1, :-1, 0, :, :, :] / (2 * alphas_sum)
+        j_range[0:1, 1:, 0, :, :, :] * integrals[0:1, :-1, 0, :, :, :] / (2 * exps_sum)
     )
     integrals[1:2, 1:, 1:, :, :, :] = rel_coord_moment * integrals[0:1, 1:, 1:, :, :, :] + (
         i_range[0:1, 0:1, 1:, :, :, :] * integrals[0:1, 1:, :-1, :, :, :]
         + j_range[0:1, 1:, 0:1, :, :, :] * integrals[0:1, :-1, 1:, :, :, :]
-    ) / (2 * alphas_sum)
+    ) / (2 * exps_sum)
     for k in range(1, order_moment_max):
         integrals[k + 1, 0, 0, :, :, :] = rel_coord_moment * integrals[k, 0, 0, :, :, :] + (
-            k * integrals[k - 1, 0, 0, :, :, :] / (2 * alphas_sum)
+            k * integrals[k - 1, 0, 0, :, :, :] / (2 * exps_sum)
         )
         integrals[k + 1, 0, 1:, :, :, :] = rel_coord_moment * integrals[k, 0, 1:, :, :, :] + (
             i_range[0, 0, 1:, :, :, :] * integrals[k, 0, :-1, :, :, :]
             + k * integrals[k - 1, 0, 1:, :, :, :]
-        ) / (2 * alphas_sum)
+        ) / (2 * exps_sum)
         integrals[k + 1, 1:, 0, :, :, :] = rel_coord_moment * integrals[k, 1:, 0, :, :, :] + (
             j_range[0, 1:, 0, :, :, :] * integrals[k, :-1, 0, :, :, :]
             + k * integrals[k - 1, 1:, 0, :, :, :]
-        ) / (2 * alphas_sum)
+        ) / (2 * exps_sum)
         integrals[k + 1, 1:, 1:, :, :, :] = rel_coord_moment * integrals[k, 1:, 1:, :, :, :] + (
             i_range[0, 0:, 1:, :, :, :] * integrals[k, 1:, :-1, :, :, :]
             + j_range[0, 1:, 0:, :, :, :] * integrals[k, :-1, 1:, :, :, :]
             + k * integrals[k - 1, 1:, 1:, :, :, :]
-        ) / (2 * alphas_sum)
+        ) / (2 * exps_sum)
 
     # select the appropriate moments and angular momentums
     integrals = integrals[
