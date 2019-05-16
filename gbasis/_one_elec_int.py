083c377ce12f39979e297692cd906d05c3dc6025
Taewon D. Kim <kimt33@mcmaster.ca>
2019-05-16 21:34:48 +0200

    Add TODO
    In the case of nuclear electron attraction or many point charge interaction (for
    solvation effect?), the _compute_one_elec_integrals will be repeated multiple
    times with only coord_point changing. This seems to suggest that we can
    vectorize over the coord_point, provided that the memory cost isn't too crazy

--- gbasis/_one_elec_int.py
+++ gbasis/_one_elec_int.py
@@ -5,6 +5,7 @@ from scipy.special import factorial2
 
 # pylint: disable=C0103,R0914,R0915
 # FIXME: returns nan when exponent is zero
+# TODO: vectorize over multiple coordinate points (i.e. nuclear electron attraction)
 def _compute_one_elec_integrals(
     coord_point, boys_func, coord_a, angmom_a, exps_a, coeffs_a, coord_b, angmom_b, exps_b, coeffs_b
 ):
