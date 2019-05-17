3e3531fdb1e8e17da7284108a20c6e5cb964cb8a
Taewon D. Kim <kimt33@mcmaster.ca>
2019-05-17 12:09:53 +0200

    Update electrostatic potential helper functions
    What:
    1. Rename one_electron_integral to electrostatic_potential
    2. Remove boys_func parameter
    3. Call PointChargeIntegral methods with the hard-coded charge
    4. Update docstring
    
    Why:
    1. More descriptive
    2. Since boys_func is available in PointChargeIntegral, we do not need to
    provide the boys_func again

--- gbasis/electrostatic_potential.py
+++ gbasis/electrostatic_potential.py
@@ -2,8 +2,8 @@
 from gbasis.point_charge import PointChargeIntegral
 
 
-def one_electron_integral_basis_cartesian(basis, coord_point, boys_func):
-    """Return the one-electron integrals of the basis set in the Cartesian form.
+def electrostatic_potential_basis_cartesian(basis, coord_point):
+    """Return the electrostatic potentials of the basis set in the Cartesian form.
 
     Parameters
     ----------
@@ -11,25 +11,22 @@ def one_electron_integral_basis_cartesian(basis, coord_point, boys_func):
         Contracted Cartesian Gaussians (of the same shell) that will be used to construct an array.
     coord_point : np.ndarray(3,)
         Center of the point charge.
-    boys_func : function(order : int, weighted_dist : np.ndarray(L_b, L_a))
-        Boys function used to evaluate the one-electron integral.
-        `L_a` and `L_b` are the angular momentum of contraction one and two respectively.
 
     Returns
     -------
-    array : np.ndarray(K_cart, K_cart, D)
+    array : np.ndarray(K_cart, K_cart)
         Array associated with the given set of contracted Cartesian Gaussians.
         First and second indices of the array are associated with the contracted Cartesian
         Gaussians. `K_cart` is the total number of Cartesian contractions within the instance.
 
     """
     return PointChargeIntegral(basis).construct_array_cartesian(
-        coord_point=coord_point, boys_func=boys_func
+        coord_point=coord_point, charge_point=-1
     )
 
 
-def one_electron_integral_basis_spherical(basis, coord_point, boys_func):
-    """Return the one-electron integrals of the basis set in the spherical form.
+def electrostatic_potential_basis_spherical(basis, coord_point):
+    """Return the electrostatic potentials of the basis set in the spherical form.
 
     Parameters
     ----------
@@ -37,13 +34,10 @@ def one_electron_integral_basis_spherical(basis, coord_point, boys_func):
         Contracted Cartesian Gaussians (of the same shell) that will be used to construct an array.
     coord_point : np.ndarray(3,)
         Center of the point charge.
-    boys_func : function(order : int, weighted_dist : np.ndarray(L_b, L_a))
-        Boys function used to evaluate the one-electron integral.
-        `L_a` and `L_b` are the angular momentum of contraction one and two respectively.
 
     Returns
     -------
-    array : np.ndarray(K_sph, K_sph, D)
+    array : np.ndarray(K_sph, K_sph)
         Array associated with the atomic orbitals associated with the given set(s) of contracted
         Cartesian Gaussians.
         First and second indices of the array are associated with two contracted spherical Gaussians
@@ -52,12 +46,12 @@ def one_electron_integral_basis_spherical(basis, coord_point, boys_func):
 
     """
     return PointChargeIntegral(basis).construct_array_spherical(
-        coord_point=coord_point, boys_func=boys_func
+        coord_point=coord_point, charge_point=-1
     )
 
 
-def one_electron_integral_spherical_lincomb(basis, transform, coord_point, boys_func):
-    """Return the one-electron integrals of the LCAO's in the spherical form.
+def electrostatic_potential_spherical_lincomb(basis, transform, coord_point):
+    """Return the electrostatic potentials of the LCAO's in the spherical form.
 
     Parameters
     ----------
@@ -69,13 +63,10 @@ def one_electron_integral_spherical_lincomb(basis, transform, coord_point, boys_
         and first index of the array for contracted spherical Gaussians.
     coord_point : np.ndarray(3,)
         Center of the point charge.
-    boys_func : function(order : int, weighted_dist : np.ndarray(L_b, L_a))
-        Boys function used to evaluate the one-electron integral.
-        `L_a` and `L_b` are the angular momentum of contraction one and two respectively.
 
     Returns
     -------
-    array : np.ndarray(K_orbs, K_orbs, D)
+    array : np.ndarray(K_orbs, K_orbs)
         Array whose first and second indices are associated with the linear combinations of the
         contracted spherical Gaussians.
         First and second indices of the array correspond to the linear combination of contracted
@@ -84,5 +75,5 @@ def one_electron_integral_spherical_lincomb(basis, transform, coord_point, boys_
 
     """
     return PointChargeIntegral(basis).construct_array_spherical_lincomb(
-        transform, coord_point=coord_point, boys_func=boys_func
+        transform, coord_point=coord_point, charge_point=-1
     )
