Fix negative sign in the one electron (point charge) integral
Turns out all charges for point charge integrals need to be multiplied with -1.

gbasis/nuclear_electron_attraction.py
@@ -81,7 +81,7 @@ class NuclearElectronAttraction(PointChargeIntegral):
 
         """
         output = super().construct_array_contraction(
-            contractions_one, contractions_two, nuclear_coords, -nuclear_charges
+            contractions_one, contractions_two, nuclear_coords, nuclear_charges
         )
         return np.sum(output, axis=4)
 

gbasis/point_charge.py
@@ -241,7 +241,7 @@ class PointChargeIntegral(BaseTwoIndexSymmetric):
         # axis 3 : angular momentum vector of contraction two (in the same order as angmoms_b)
         # axis 4 : point charge
         output = (
-            charges_points
+            -charges_points
             * integrals[
                 np.arange(coeffs_a.shape[1])[:, None, None, None, None],
                 angmoms_a_x[None, :, None, None, None],