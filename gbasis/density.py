Fix shape of the gradient of density
There are three components to the gradient, one for each of x, y, and z.

gbasis/density.py
@@ -165,7 +165,7 @@ def eval_density_gradient(one_density_matrix, basis, coords, transform):
 
     Returns
     -------
-    density_gradient : np.ndarray(N,)
+    density_gradient : np.ndarray(N, 3)
         Gradient of the density evaluated at the given coordinates.
 
     """
@@ -175,4 +175,5 @@ def eval_density_gradient(one_density_matrix, basis, coords, transform):
             eval_deriv_density(np.array([0, 1, 0]), one_density_matrix, basis, coords, transform),
             eval_deriv_density(np.array([0, 0, 1]), one_density_matrix, basis, coords, transform),
         ]
+    ).T
     )

tests/test_density.py
@@ -233,7 +233,7 @@ def test_eval_density_gradient():
     coords = np.random.rand(10, 3)
 
     np.allclose(
-        eval_density_gradient(density, basis, coords, transform),
+        eval_density_gradient(density, basis, coords, transform).T,
         np.array(
             [
                 np.einsum(