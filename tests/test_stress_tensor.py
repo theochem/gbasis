f7202367be9f042d9bc80134a30c83932e7c1695
Taewon D. Kim <kimt33@mcmaster.ca>
2019-06-03 09:35:11 +0200

    Add Ehrenfest force
    Issues:
    No test against reference

--- tests/test_stress_tensor.py
+++ tests/test_stress_tensor.py
@@ -1,7 +1,7 @@
 """Test gbasis.stress_tensor."""
 from gbasis.density import eval_density_laplacian, eval_deriv_density, eval_deriv_density_matrix
 from gbasis.parsers import make_contractions, parse_nwchem
-from gbasis.stress_tensor import eval_stress_tensor
+from gbasis.stress_tensor import eval_ehrenfest_force, eval_stress_tensor
 import numpy as np
 import pytest
 from utils import find_datafile, HortonContractions
@@ -71,3 +71,66 @@ def test_eval_stress_tensor():
     assert np.allclose(test_b, np.swapaxes(test_b, 1, 2))
     assert np.allclose(test_c, np.swapaxes(test_c, 1, 2))
     assert np.allclose(test_d, np.swapaxes(test_d, 1, 2))
+
+
+def test_eval_ehrenfest_force():
+    """Test gbasis.stress_tensor.eval_ehrenfest_force."""
+    with open(find_datafile("data_anorcc.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+    coords = np.array([[0, 0, 0]])
+    basis = make_contractions(basis_dict, ["H"], coords)
+    points = np.random.rand(10, 3)
+
+    with pytest.raises(TypeError):
+        eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), np.array(0), 0)
+    with pytest.raises(TypeError):
+        eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 1, None)
+    with pytest.raises(TypeError):
+        eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 1, 0j)
+
+    test_a = eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 0, 0)
+    test_b = eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 1, 0)
+    test_c = eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 0.5, 0)
+    test_d = eval_ehrenfest_force(np.identity(40), basis, points, np.identity(40), 0, 2)
+    for j in range(3):
+        ref_a = np.zeros(points.shape[0])
+        ref_b = np.zeros(points.shape[0])
+        ref_c = np.zeros(points.shape[0])
+        ref_d = np.zeros(points.shape[0])
+
+        orders_j = np.array([0, 0, 0])
+        orders_j[j] += 1
+        for i in range(3):
+            orders_i = np.array([0, 0, 0])
+            orders_i[i] += 1
+
+            temp1 = eval_deriv_density_matrix(
+                2 * orders_i, orders_j, np.identity(40), basis, points, np.identity(40)
+            )
+            temp2 = eval_deriv_density_matrix(
+                orders_i, orders_i + orders_j, np.identity(40), basis, points, np.identity(40)
+            )
+            temp3 = eval_deriv_density_matrix(
+                2 * orders_i + orders_j,
+                np.array([0, 0, 0]),
+                np.identity(40),
+                basis,
+                points,
+                np.identity(40),
+            )
+            temp4 = eval_deriv_density_matrix(
+                orders_i + orders_j, orders_i, np.identity(40), basis, points, np.identity(40)
+            )
+            temp5 = eval_deriv_density(
+                2 * orders_i + orders_j, np.identity(40), basis, points, np.identity(40)
+            )
+
+            ref_a += temp3 + temp4
+            ref_b += -temp1 - temp2
+            ref_c += -0.5 * temp1 - 0.5 * temp2 + 0.5 * temp3 + 0.5 * temp4
+            ref_d += temp3 + temp4 - temp5
+        assert np.allclose(test_a[:, j], ref_a)
+        assert np.allclose(test_b[:, j], ref_b)
+        assert np.allclose(test_c[:, j], ref_c)
+        assert np.allclose(test_d[:, j], ref_d)
