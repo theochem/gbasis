e8a1bdde5e68f7d060669d439a69502a1823c092
Taewon D. Kim <kimt33@mcmaster.ca>
2019-06-03 09:41:15 +0200

    Add Ehrenfest Hessian
    Issues:
    No test against reference

--- gbasis/stress_tensor.py
+++ gbasis/stress_tensor.py
@@ -248,3 +248,186 @@ def eval_ehrenfest_force(
                     )
                 )
     return output.T
+
+
+# TODO: need to be tested against reference
+def eval_ehrenfest_hessian(
+    one_density_matrix,
+    basis,
+    coords,
+    transform,
+    alpha=1,
+    beta=0,
+    coord_type="spherical",
+    symmetric=False,
+):
+    r"""Return the Ehrenfest Hessian.
+
+    Ehrenfest Hessian is the gradient of the Ehrenfest force:
+    .. math::
+
+        H_{jk}(\mathbf{r}_n | \alpha, \beta)
+        =&
+        \frac{\partial}{\partial r_k} F_j(\mathbf{r}_n | \alpha, \beta)\\
+        =&
+        - \alpha
+        \sum_i
+        \left(
+            \frac{\partial^4}{\partial r^2_i \partial r_k \partial r'_j}
+            \gamma(\mathbf{r}, \mathbf{r}')
+            +\frac{\partial^4}{\partial r^2_i \partial r'_j \partial r'_k}
+            \gamma(\mathbf{r}, \mathbf{r}')
+        \right)_{\mathbf{r} = \mathbf{r}' = \mathbf{r}_n}\\
+        &+ (1 - \alpha)
+        \sum_i
+        \left(
+            \frac{\partial^4}{\partial r^2_i \partial r_j \partial r_k}
+            \gamma(\mathbf{r}, \mathbf{r})
+            + \frac{\partial^4}{\partial r^2_i \partial r_j \partial r'_k}
+            \gamma(\mathbf{r}, \mathbf{r})
+        \right)_{\mathbf{r} = \mathbf{r}' = \mathbf{r}_n}\\
+        &+ (1 - 2\alpha)
+        \sum_i
+        \left(
+            \frac{\partial^4}{\partial r_i \partial r_j \partial r_k \partial r'_i}
+            \gamma(\mathbf{r}, \mathbf{r})
+            + \frac{\partial^4}{\partial r_i \partial r_j \partial r'_i \partial r'_k}
+            \gamma(\mathbf{r}, \mathbf{r})
+        \right)_{\mathbf{r} = \mathbf{r}' = \mathbf{r}_n}\\
+        &- \frac{1}{2} \beta
+        \left.
+            \left(
+                \frac{\partial^4}{\partial r_j \partial r_k \partial x^2}
+                + \frac{\partial^4}{\partial r_j \partial r_k \partial y^2}
+                + \frac{\partial^4}{\partial r_j \partial r_k \partial z^2}
+            \right)
+            \rho(\mathbf{r}_n)
+        \right|_{\mathbf{r}=\mathbf{r}_n}\\
+
+    Parameters
+    ----------
+    one_density_matrix : np.ndarray(K_orb, K_orb)
+        One-electron density matrix.
+    basis : list/tuple of GeneralizedContractionShell
+        Contracted Cartesian Gaussians (of the same shell) that will be used to construct an array.
+    coords : np.ndarray(N, 3)
+        Points in space where the contractions are evaluated.
+    transform : np.ndarray(K_orbs, K_cont)
+        Transformation matrix from contractions in the given coordinate system (e.g. AO) to linear
+        combinations of contractions (e.g. MO).
+        Transformation is applied to the left.
+        Rows correspond to the linear combinationes (i.e. MO) and the columns correspond to the
+        contractions (i.e. AO).
+    alpha : {int, float}
+        First parameter of the stress tensor.
+        Default value is 1.
+    beta : {int, float}
+        Second parameter of the stress tensor.
+        Default value is 0.
+    coord_type : {"cartesian", list/tuple of "cartesian" or "spherical", "spherical"}
+        Types of the coordinate system for the contractions.
+        If "cartesian", then all of the contractions are treated as Cartesian contractions.
+        If "spherical", then all of the contractions are treated as spherical contractions.
+        If list/tuple, then each entry must be a "cartesian" or "spherical" to specify the
+        coordinate type of each GeneralizedContractionShell instance.
+        Default value is "spherical".
+    symmetric : {True, False}
+        Flag for symmeterizing the Hessian.
+        If True, then the Hessian is symmeterized by averaging it with its transpose.
+        Default value is False.
+
+    Returns
+    -------
+    ehrenfest_hessian : np.ndarray(N, 3, 3)
+        Ehrenfest Hessian of the given density matrix at the given coordinates.
+        Hessian is symmeterized if `symmetric` is True.
+
+    Raises
+    ------
+    TypeError
+        If `alpha` is not an integer or float.
+        If `beta` is not an integer or float.
+
+    """
+    if not isinstance(alpha, (int, float)):
+        raise TypeError("`alpha` must be an integer or a float.")
+    if not isinstance(beta, (int, float)):
+        raise TypeError("`beta` must be an integer or a float.")
+    output = np.zeros((3, 3, coords.shape[0]))
+    for i, orders_two in enumerate(np.identity(3, dtype=int)):
+        for j, orders_three in enumerate(np.identity(3, dtype=int)):
+            for orders_one in np.identity(3, dtype=int):
+                if alpha != 0:
+                    output[i, j] -= alpha * eval_deriv_density_matrix(
+                        2 * orders_one + orders_three,
+                        orders_two,
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                    output[i, j] -= alpha * eval_deriv_density_matrix(
+                        2 * orders_one,
+                        orders_two + orders_three,
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                if alpha != 1:
+                    output[i, j] += (1 - alpha) * eval_deriv_density_matrix(
+                        2 * orders_one + orders_two + orders_three,
+                        np.array([0, 0, 0]),
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                    output[i, j] += (1 - alpha) * eval_deriv_density_matrix(
+                        2 * orders_one + orders_two,
+                        orders_three,
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                if alpha != 0.5:
+                    output[i, j] += (1 - 2 * alpha) * eval_deriv_density_matrix(
+                        orders_one + orders_two + orders_three,
+                        orders_one,
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                    output[i, j] += (1 - 2 * alpha) * eval_deriv_density_matrix(
+                        orders_one + orders_two,
+                        orders_one + orders_three,
+                        one_density_matrix,
+                        basis,
+                        coords,
+                        transform,
+                        coord_type=coord_type,
+                    )
+                if beta != 0:
+                    output[i, j] -= (
+                        0.5
+                        * beta
+                        * eval_deriv_density(
+                            2 * orders_one + orders_two + orders_three,
+                            one_density_matrix,
+                            basis,
+                            coords,
+                            transform,
+                            coord_type=coord_type,
+                        )
+                    )
+    if symmetric:
+        output += np.swapaxes(output, 0, 1)
+        output /= 2
+    return np.transpose(output, (2, 0, 1))
--- tests/test_stress_tensor.py
+++ tests/test_stress_tensor.py
@@ -1,7 +1,7 @@
 """Test gbasis.stress_tensor."""
 from gbasis.density import eval_density_laplacian, eval_deriv_density, eval_deriv_density_matrix
 from gbasis.parsers import make_contractions, parse_nwchem
-from gbasis.stress_tensor import eval_ehrenfest_force, eval_stress_tensor
+from gbasis.stress_tensor import eval_ehrenfest_force, eval_ehrenfest_hessian, eval_stress_tensor
 import numpy as np
 import pytest
 from utils import find_datafile, HortonContractions
@@ -134,3 +134,143 @@ def test_eval_ehrenfest_force():
         assert np.allclose(test_b[:, j], ref_b)
         assert np.allclose(test_c[:, j], ref_c)
         assert np.allclose(test_d[:, j], ref_d)
+
+
+def test_eval_ehrenfest_hessian():
+    """Test gbasis.stress_tensor.eval_ehrenfest_hessian."""
+    with open(find_datafile("data_anorcc.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+    coords = np.array([[0, 0, 0]])
+    basis = make_contractions(basis_dict, ["H"], coords)
+    basis = [HortonContractions(i.angmom, i.coord, i.coeffs, i.exps) for i in basis]
+
+    points = np.random.rand(10, 3)
+
+    with pytest.raises(TypeError):
+        eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), np.array(0), 0)
+    with pytest.raises(TypeError):
+        eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 1, None)
+    with pytest.raises(TypeError):
+        eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 1, 0j)
+
+    test_a = eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 0, 0)
+    test_b = eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 1, 0)
+    test_c = eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 0.5, 0)
+    test_d = eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 0, 2)
+    for j in range(3):
+        for k in range(3):
+            ref_a = np.zeros(points.shape[0])
+            ref_b = np.zeros(points.shape[0])
+            ref_c = np.zeros(points.shape[0])
+            ref_d = np.zeros(points.shape[0])
+
+            orders_j = np.array([0, 0, 0])
+            orders_j[j] += 1
+            orders_k = np.array([0, 0, 0])
+            orders_k[k] += 1
+            for i in range(3):
+                orders_i = np.array([0, 0, 0])
+                orders_i[i] += 1
+
+                temp1 = eval_deriv_density_matrix(
+                    2 * orders_i + orders_k,
+                    orders_j,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp2 = eval_deriv_density_matrix(
+                    2 * orders_i,
+                    orders_j + orders_k,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp3 = eval_deriv_density_matrix(
+                    orders_i + orders_k,
+                    orders_i + orders_j,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp4 = eval_deriv_density_matrix(
+                    orders_i,
+                    orders_i + orders_j + orders_k,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp5 = eval_deriv_density_matrix(
+                    2 * orders_i + orders_j + orders_k,
+                    np.array([0, 0, 0]),
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp6 = eval_deriv_density_matrix(
+                    2 * orders_i + orders_j,
+                    orders_k,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp7 = eval_deriv_density_matrix(
+                    orders_i + orders_j + orders_k,
+                    orders_i,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp8 = eval_deriv_density_matrix(
+                    orders_i + orders_j,
+                    orders_i + orders_k,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+                temp9 = eval_deriv_density(
+                    2 * orders_i + orders_j + orders_k,
+                    np.identity(40),
+                    basis,
+                    points,
+                    np.identity(40),
+                )
+
+                ref_a += temp5 + temp6 + temp7 + temp8
+                ref_b += -temp1 - temp2 - temp3 - temp4
+                ref_c += (
+                    -0.5 * temp1
+                    - 0.5 * temp2
+                    - 0.5 * temp3
+                    - 0.5 * temp4
+                    + 0.5 * temp5
+                    + 0.5 * temp6
+                    + 0.5 * temp7
+                    + 0.5 * temp8
+                )
+                ref_d += temp3 + temp4 + temp5 + temp6 - temp9
+            assert np.allclose(test_a[:, j, k], ref_a)
+            assert np.allclose(test_b[:, j, k], ref_b)
+            assert np.allclose(test_c[:, j, k], ref_c)
+            assert np.allclose(test_d[:, j, k], ref_d)
+    assert np.allclose(
+        eval_ehrenfest_hessian(
+            np.identity(40), basis, points, np.identity(40), 0, 0, symmetric=True
+        ),
+        (
+            eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 0, 0)
+            + np.swapaxes(
+                eval_ehrenfest_hessian(np.identity(40), basis, points, np.identity(40), 0, 0), 1, 2
+            )
+        )
+        / 2,
+    )
