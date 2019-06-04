f9ff7ebe76654ebe187f314ea271722251d8ec1f
Taewon D. Kim <kimt33@mcmaster.ca>
2019-06-04 22:35:34 +0200

    Add interface to iodata module
    Issues:
    1. I cannot understand the convention used for the spherical contraction/primitive
    ordering.
    2. Didn't want dependency on iodata so the input is checked by class and
    attribute names
    3. The API of iodata used has not been updated on pypi nor on github (not
    tagged).

---
+++ tests/test_wrappers.py
@@ -0,0 +1,59 @@
+"""Test gbasis.wrapper."""
+from gbasis.contractions import GeneralizedContractionShell
+from gbasis.wrappers import from_iodata
+import numpy as np
+import pytest
+from utils import find_datafile
+
+
+def test_from_iodata():
+    """Test gbasis.wrapper.from_iodata."""
+    pytest.importorskip("iodata")
+    from iodata import load_one
+
+    mol = load_one(find_datafile("data_iodata_water_sto3g_hf_g03.fchk"))
+
+    basis = from_iodata(mol)
+
+    assert all(isinstance(i, GeneralizedContractionShell) for i in basis)
+    assert basis[0].angmom == 0
+    assert np.allclose(basis[0].coord, mol.atcoords[0])
+    assert np.allclose(basis[0].exps, np.array([130.7093214, 23.80886605, 6.443608313]))
+    assert np.allclose(
+        basis[0].coeffs, np.array([0.1543289673, 0.5353281423, 0.4446345422]).reshape(-1, 1)
+    )
+
+    assert basis[1].angmom == 0
+    assert np.allclose(basis[1].coord, mol.atcoords[0])
+    assert np.allclose(basis[1].exps, np.array([5.033151319, 1.169596125, 0.3803889600]))
+    assert np.allclose(
+        basis[1].coeffs, np.array([-0.09996722919, 0.3995128261, 0.7001154689]).reshape(-1, 1)
+    )
+
+    assert basis[2].angmom == 1
+    assert np.allclose(basis[2].coord, mol.atcoords[0])
+    assert np.allclose(basis[2].exps, np.array([5.033151319, 1.169596125, 0.3803889600]))
+    assert np.allclose(
+        basis[2].coeffs, np.array([0.1559162750, 0.6076837186, 0.3919573931]).reshape(-1, 1)
+    )
+
+    assert basis[3].angmom == 0
+    assert np.allclose(basis[3].coord, mol.atcoords[1])
+    assert np.allclose(basis[3].exps, np.array([3.425250914, 0.6239137298, 0.1688554040]))
+    assert np.allclose(
+        basis[3].coeffs, np.array([0.1543289673, 0.5353281423, 0.4446345422]).reshape(-1, 1)
+    )
+
+    assert basis[4].angmom == 0
+    assert np.allclose(basis[4].coord, mol.atcoords[2])
+    assert np.allclose(basis[4].exps, np.array([3.425250914, 0.6239137298, 0.1688554040]))
+    assert np.allclose(
+        basis[4].coeffs, np.array([0.1543289673, 0.5353281423, 0.4446345422]).reshape(-1, 1)
+    )
+
+    with pytest.raises(NotImplementedError):
+        basis[2].angmom_components_sph  # pylint: disable=W0104
+
+    with pytest.raises(ValueError):
+        mol.obasis = mol.obasis._replace(primitive_normalization="L1")
+        mol.gbasis_basis()
