42a777601abbb2dd025c58e49bf219fd6625ac9e
Taewon D. Kim <kimt33@mcmaster.ca>
2019-06-05 21:54:55 +0200

    Add overlap between two basis sets

---
+++ gbasis/overlap_asymm.py
@@ -0,0 +1,120 @@
+"""Functions for computing overlap between two basis sets."""
+from gbasis.base_two_asymm import BaseTwoIndexAsymmetric
+from gbasis.overlap import Overlap
+
+
+class OverlapAsymmetric(BaseTwoIndexAsymmetric):
+    """Class for obtaining the overlap between two sets of Gaussian contractions.
+
+    Attributes
+    ----------
+    _axes_contractions : tuple of tuple of GeneralizedContractionShell
+        Sets of contractions associated with each axis of the array.
+
+    Properties
+    ----------
+    contractions_one : tuple of GeneralizedContractionShell
+        Contractions that are associated with the first index of the array.
+    contractions_two : tuple of GeneralizedContractionShell
+        Contractions that are associated with the second index of the array.
+
+    Methods
+    -------
+    __init__(self, contractions_one, contractions_two)
+        Initialize.
+    construct_array_contraction(contractions_one, contractions_two) :
+    np.ndarray(M_1, L_cart_1, M_2, L_cart_2)
+        Return the overlap associated with the two `GeneralizedContractionShell` instances.
+        `M_1` is the number of segmented contractions with the same exponents (and angular momentum)
+        associated with the first index.
+        `L_cart_1` is the number of Cartesian contractions for the given angular momentum associated
+        with the first index.
+        `M_2` is the number of segmented contractions with the same exponents (and angular momentum)
+        associated with the second index.
+        `L_cart_2` is the number of Cartesian contractions for the given angular momentum associated
+        with the second index.
+    construct_array_cartesian(self) : np.ndarray(K_cart_1, K_cart_2)
+        Return the overlap integrals between Cartesian Gaussians of the two basis sets.
+        `K_cart_1` is the total number of Cartesian contractions within the `contractions_one`.
+        `K_cart_2` is the total number of Cartesian contractions within the `contractions_two`.
+    construct_array_spherical(self) : np.ndarray(K_sph_1, K_sph_2)
+        Return the overlap integrals associated with spherical Gaussians (atomic orbitals) of the
+        two basis sets.
+        `K_sph_1` is the total number of spherical contractions within the `contractions_one`.
+        `K_sph_2` is the total number of spherical contractions within the `contractions_two`.
+    construct_array_mix(self, coord_types_one, coord_types_two) :
+    np.ndarray(K_cont_1, K_cont_2)
+        Return the overlap integrals associated with the contractions in the given coordinate
+        systems of the two basis sets.
+        `K_cont_1` is the total number of contractions within the given basis set.
+        `K_cont_2` is the total number of contractions within the given basis set.
+    construct_array_lincomb(self, transform_one, transform_two, coord_type_one, coord_type_two) :
+    np.ndarray(K_orbs_1, K_orbs_2)
+        Return the overlap integrals associated with the linear combinations of contractions of the
+        two basis sets.
+        `K_orbs_1` is the number of basis functions produced after the linear combinations of the
+        spherical contractions associated with `contractions_one`.
+        `K_orbs_2` is the number of basis functions produced after the linear combinations of the
+        spherical contractions associated with `contractions_two`.
+
+    """
+
+    construct_array_contraction = staticmethod(Overlap.construct_array_contraction)
+
+
+def overlap_integral_asymmetric(
+    basis_one,
+    basis_two,
+    transform_one=None,
+    transform_two=None,
+    coord_type_one="spherical",
+    coord_type_two="spherical",
+):
+    """Return overlap integrals between two basis sets.
+
+    Parameters
+    ----------
+    basis_one : list/tuple of GeneralizedContractionShell
+        Shells of generalized contractions.
+    basis_two : list/tuple of GeneralizedContractionShell
+        Shells of generalized contractions.
+    transform_one : np.ndarray(K_orbs, K_cont)
+        Transformation matrix of the `basis_one` in the given coordinate system (e.g. AO) to linear
+        combinations of contractions (e.g. MO).
+        Transformation is applied to the left, i.e. the sum is over the index 1 of `transform`
+        and index 0 of the array for contractions.
+        Default is no transformation.
+    transform_one : np.ndarray(K_orbs, K_cont)
+        Transformation matrix of the `basis_two` in the given coordinate system (e.g. AO) to linear
+        combinations of contractions (e.g. MO).
+        Transformation is applied to the left, i.e. the sum is over the index 1 of `transform`
+        and index 0 of the array for contractions.
+        Default is no transformation.
+    coord_type_one : {"cartesian", list/tuple of "cartesian" or "spherical", "spherical"}
+        Types of the coordinate system for the contractions in `basis_one`.
+        If "cartesian", then all of the contractions are treated as Cartesian contractions.
+        If "spherical", then all of the contractions are treated as spherical contractions.
+        If list/tuple, then each entry must be a "cartesian" or "spherical" to specify the
+        coordinate type of each GeneralizedContractionShell instance.
+        Default value is "spherical".
+    coord_type_two : {"cartesian", list/tuple of "cartesian" or "spherical", "spherical"}
+        Types of the coordinate system for the contractions in `basis_two`.
+        If "cartesian", then all of the contractions are treated as Cartesian contractions.
+        If "spherical", then all of the contractions are treated as spherical contractions.
+        If list/tuple, then each entry must be a "cartesian" or "spherical" to specify the
+        coordinate type of each GeneralizedContractionShell instance.
+        Default value is "spherical".
+
+    Returns
+    -------
+    array : np.ndarray(K_orbs_1, K_orbs_2)
+        Overlap integral of the given basis set.
+        Dimensions 0 of the array correspond to the basis functions in `basis_one`. `K_orbs_1` is
+        the number of basis functions in the `basis_one`.
+        Dimensions 1 of the array correspond to the basis functions in `basis_two`. `K_orbs_2` is
+        the number of basis functions in the `basis_two`.
+
+    """
+    return OverlapAsymmetric(basis_one, basis_two).construct_array_lincomb(
+        transform_one, transform_two, coord_type_one, coord_type_two
+    )
---
+++ tests/test_overlap_asymm.py
@@ -0,0 +1,97 @@
+"""Test gbasis.overlap_asymm."""
+from gbasis.overlap import overlap_integral
+from gbasis.overlap_asymm import overlap_integral_asymmetric
+from gbasis.parsers import make_contractions, parse_nwchem
+import numpy as np
+from utils import find_datafile, HortonContractions
+
+
+def test_overlap_integral_asymmetric_horton_anorcc_hhe():
+    """Test gbasis.eval.overlap_integral_asymmetric against HORTON's overlap matrix.
+
+    The test case is diatomic with H and He separated by 0.8 angstroms with basis set ANO-RCC.
+
+    """
+    with open(find_datafile("data_anorcc.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+    # NOTE: used HORTON's conversion factor for angstroms to bohr
+    basis = make_contractions(
+        basis_dict, ["H", "He"], np.array([[0, 0, 0], [0.8 * 1.0 / 0.5291772083, 0, 0]])
+    )
+    basis = [HortonContractions(i.angmom, i.coord, i.coeffs, i.exps) for i in basis]
+
+    horton_overlap_integral_asymmetric = np.load(find_datafile("data_horton_hhe_cart_overlap.npy"))
+    assert np.allclose(
+        overlap_integral_asymmetric(
+            basis, basis, coord_type_two="cartesian", coord_type_one="cartesian"
+        ),
+        horton_overlap_integral_asymmetric,
+    )
+
+
+def test_overlap_integral_asymmetric_horton_anorcc_bec():
+    """Test gbasis.eval.overlap_integral_asymmetric against HORTON's overlap matrix.
+
+    The test case is diatomic with Be and C separated by 1.0 angstroms with basis set ANO-RCC.
+
+    """
+    with open(find_datafile("data_anorcc.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+    # NOTE: used HORTON's conversion factor for angstroms to bohr
+    basis = make_contractions(
+        basis_dict, ["Be", "C"], np.array([[0, 0, 0], [1.0 * 1.0 / 0.5291772083, 0, 0]])
+    )
+    basis = [HortonContractions(i.angmom, i.coord, i.coeffs, i.exps) for i in basis]
+
+    horton_overlap_integral_asymmetric = np.load(find_datafile("data_horton_bec_cart_overlap.npy"))
+    assert np.allclose(
+        overlap_integral_asymmetric(
+            basis, basis, coord_type_two="cartesian", coord_type_one="cartesian"
+        ),
+        horton_overlap_integral_asymmetric,
+    )
+
+
+def test_overlap_integral_asymmetric_compare():
+    """Test gbasis.eval.overlap against gbasis.overlap.overlap_integral."""
+    with open(find_datafile("data_anorcc.nwchem"), "r") as f:
+        test_basis = f.read()
+    basis_dict = parse_nwchem(test_basis)
+
+    basis = make_contractions(basis_dict, ["Kr", "Kr"], np.array([[0, 0, 0], [1.0, 0, 0]]))
+    basis = [HortonContractions(i.angmom, i.coord, i.coeffs, i.exps) for i in basis]
+
+    assert np.allclose(
+        overlap_integral(basis, coord_type="cartesian"),
+        overlap_integral_asymmetric(
+            basis, basis, coord_type_one="cartesian", coord_type_two="cartesian"
+        ),
+    )
+    assert np.allclose(
+        overlap_integral(basis, coord_type="spherical"),
+        overlap_integral_asymmetric(
+            basis, basis, coord_type_one="spherical", coord_type_two="spherical"
+        ),
+    )
+    assert np.allclose(
+        overlap_integral(basis, transform=np.identity(218), coord_type="spherical"),
+        overlap_integral_asymmetric(
+            basis,
+            basis,
+            transform_one=np.identity(218),
+            transform_two=np.identity(218),
+            coord_type_one="spherical",
+            coord_type_two="spherical",
+        ),
+    )
+    assert np.allclose(
+        overlap_integral(basis, coord_type=["spherical"] * 9 + ["cartesian"]),
+        overlap_integral_asymmetric(
+            basis,
+            basis,
+            coord_type_one=["spherical"] * 9 + ["cartesian"],
+            coord_type_two=["spherical"] * 9 + ["cartesian"],
+        ),
+    )
