c82b730860d185125bfe461a47993396351262b6
Michelle Richer <michellericher93@gmail.com>
2023-11-15 11:02:09 -0500

    Add consistency tests for ovlp; UGBS seems to work

--- tests/test_libcint.py
+++ tests/test_libcint.py
@@ -152,6 +152,7 @@ def test_overlap_horton_anorcc_hhe():
 
     print(cbasis_olp)
 
+    assert np.allclose(np.diag(cbasis_olp), 1)
     assert np.allclose(cbasis_olp, gbasis_olp)
 
 
@@ -179,4 +180,61 @@ def test_overlap_horton_ugbs_bec():
 
     print(cbasis_olp)
 
+    assert np.allclose(np.diag(cbasis_olp), 1)
     assert np.allclose(cbasis_olp, gbasis_olp)
+
+
+# def test_overlap_anorcc_hhe():
+#     """Test gbasis.integrals.libcint.CBasis.olp against GBasis's overlap matrix.
+#
+#     The test case is diatomic with H and He separated by 0.8 angstroms with basis set ANO-RCC.
+#
+#     """
+#     basis_dict = parse_nwchem(find_datafile("data_anorcc.nwchem"))
+#
+#     atsyms = ["H", "He"]
+#
+#     atcoords = np.array([[0., 0., 0.], [0.8, 0., 0.]]) / 0.5291772083
+#
+#     gbasis = make_contractions(basis_dict, atsyms, atcoords)
+#     gbasis_olp = overlap_integral(gbasis, coord_type="cartesian")
+#
+#     cbasis = CBasis(gbasis, atsyms, atcoords, coord_type="cartesian")
+#     cbasis_olp = cbasis.olp()
+#
+#     assert cbasis_olp.shape[0] == cbasis_olp.shape[1] == \
+#            gbasis_olp.shape[0] == gbasis_olp.shape[1] == \
+#            cbasis.nbas
+#
+#     print(cbasis_olp)
+#
+#     assert np.allclose(np.diag(cbasis_olp), 1)
+#     assert np.allclose(cbasis_olp, gbasis_olp)
+
+
+# def test_overlap_horton_anorcc_bec():
+#     """Test gbasis.integrals.libcint.CBasis.olp against GBasis's overlap matrix.
+#
+#     The test case is diatomic with Be and C separated by 1.0 angstroms with basis set ANO-RCC.
+#
+#     """
+#     basis_dict = parse_nwchem(find_datafile("data_anorcc.nwchem"))
+#
+#     atsyms = ["Be", "C"]
+#
+#     atcoords = np.array([[0., 0., 0.], [1.0, 0., 0.]]) / 0.5291772083
+#
+#     gbasis = make_contractions(basis_dict, atsyms, atcoords)
+#     gbasis_olp = overlap_integral(gbasis, coord_type="cartesian")
+#
+#     cbasis = CBasis(gbasis, atsyms, atcoords, coord_type="cartesian")
+#     cbasis_olp = cbasis.olp()
+#
+#     assert cbasis_olp.shape[0] == cbasis_olp.shape[1] == \
+#            gbasis_olp.shape[0] == gbasis_olp.shape[1] == \
+#            cbasis.nbas
+#
+#     print(cbasis_olp)
+#
+#     assert np.allclose(np.diag(cbasis_olp), 1)
+#     assert np.allclose(cbasis_olp, gbasis_olp)
