220dd14fda06d8187acb306c9be12b2df9267067
Michelle Richer <michellericher93@gmail.com>
2023-11-29 11:43:27 -0500

    Improve build script (cleanup, CMake flags)

--- tools/install_libcint.sh
+++ tools/install_libcint.sh
@@ -2,8 +2,8 @@
 
 # Notice for user
 echo ''
-echo '    NOTE: If this script has been run previously, and it failed, please'
-echo '          remove the `build/libcint` directory before running it again.'
+echo '    NOTE: If this script has been run previously, and it failed, please ensure'
+echo '          the `build/libcint` directory is removed before running it again.'
 echo ''
 echo '    See the GBasis documentation for help:'
 echo ''
@@ -14,11 +14,25 @@ echo ''
 set -e
 
 # Get GBasis base directory name unambiguously
-gbasis_dir=$(cd "$(dirname $(dirname ${0}))" && pwd)
+gbasis_dir=$(cd "$(dirname "$(dirname "${0}")")" && pwd)
+
+# Cleanup function; runs on failure or exit
+cleanup() {
+    rm -rf "${gbasis_dir}/build/libcint"
+}
+trap cleanup EXIT
+
+# Set Libcint CMake options
+lc_cmake_opts=
+lc_cmake_opts+=' -DWITH_FORTRAN=0'
+lc_cmake_opts+=' -DWITH_CINT2_INTERFACE=1'
+lc_cmake_opts+=' -DWITH_RANGE_COULOMB=1'
+lc_cmake_opts+=' -DWITH_F12=1'
+lc_cmake_opts+=' -DKEEP_GOING=1'
 
 # Ensure build directory exists and enter it
-mkdir -p ${gbasis_dir}/build
-cd ${gbasis_dir}/build
+mkdir -p "${gbasis_dir}/build"
+cd "${gbasis_dir}/build"
 
 # Clone Libcint Git repo and enter it
 git clone http://github.com/sunqm/libcint.git
@@ -27,7 +41,7 @@ cd libcint
 # Setup Libcint build
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${gbasis_dir}/gbasis/integrals ..
+cmake "-DCMAKE_INSTALL_PREFIX=${gbasis_dir}/gbasis/integrals" ${lc_cmake_opts} ..
 
 # Compile and install Libcint
 cmake --build .
