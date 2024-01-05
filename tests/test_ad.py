17713c30fc3e41085db9fabebcbfb63af3f31126
= <valerachuiko@gmail.com>
2024-01-05 15:22:00 -0500

    added test for the nuclear derivative

--- tests/test_ad.py
+++ tests/test_ad.py
@@ -1,10 +1,12 @@
 """Test the ad module."""
 import pytest
 import numpy as np
-from gbasis.gradient.ad import eval_nuc_deriv, eval_contractions
+from gbasis.evals._deriv import _eval_deriv_contractions
+from gbasis.gradient.ad import eval_nuc_deriv, _eval_nuc_deriv, eval_contractions
 
 
 def test_eval_nuc_deriv():
+    """Test the _eval_nuc_deriv function."""
     coords = np.random.rand(10, 3)
     center = np.random.rand(3)
     R_x, R_y, R_z = center
@@ -16,7 +18,7 @@ def test_eval_nuc_deriv():
 
     output = np.apply_along_axis(eval_contractions, 1, coords, R_x, R_y, R_z, angmom_comps, alphas, prim_coeffs, norm)
 
-    gradient = eval_nuc_deriv(coords, center, angmom_comps, alphas, prim_coeffs, norm)
+    gradient = _eval_nuc_deriv(coords, center, angmom_comps, alphas, prim_coeffs, norm)
 
     dx = 1e-5
     grad_numerical_x = (np.apply_along_axis(eval_contractions, 1, coords, R_x+dx, R_y, R_z, angmom_comps, alphas, prim_coeffs, norm) -\
@@ -29,3 +31,24 @@ def test_eval_nuc_deriv():
     grad_numerical = np.hstack([grad_numerical_x, grad_numerical_y, grad_numerical_z])
 
     np.testing.assert_allclose(gradient, grad_numerical)
+
+def test_eval_nuc_deriv_full():
+    """Test the eval_nuc_deriv function."""
+    coords = np.random.rand(10, 3)
+    center = np.random.rand(3)
+
+    R_x, R_y, R_z = center
+    angmom_comps = np.random.randint(0, 2, (2, 3))
+    alphas = np.random.rand(4)
+
+    prim_coeffs = np.random.rand(4, 5)
+    norms = np.ones((2, 4))
+
+    output = eval_nuc_deriv(coords, center, angmom_comps, alphas, prim_coeffs, norms)
+
+    for i in range(3):
+        dx = np.zeros(3)
+        dx[i] = 1e-5
+        grad_numerical = (_eval_deriv_contractions(coords, np.array([0, 0, 0]), center+dx, angmom_comps, alphas, prim_coeffs, norms) -\
+            _eval_deriv_contractions(coords, np.array([0, 0, 0]), center-dx, angmom_comps, alphas, prim_coeffs, norms))/(2*dx[i])
+    print(np.allclose(output[:, :, :, i], grad_numerical))
