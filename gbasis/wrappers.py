14962894fc5b83a331eaa9d7dc263f54c59004cb
leila-pujal <leila946@hotmail.com>
2024-04-11 13:12:35 -0400

    Add function to get permutation with LibCint order

--- gbasis/wrappers.py
+++ gbasis/wrappers.py
@@ -3,7 +3,27 @@ from gbasis.contractions import GeneralizedContractionShell
 import numpy as np
 
 
+CONVENTIONS_LIBCINT = {
+    (5, 'p'): ['s5','s4', 's3', 's2', 's1', 'c0', 'c1', 'c2', 'c3', 'c4','c5'],
+    (4, 'p'): ['s3', 's2', 's1', 'c0', 'c1', 'c2', 'c3'],
+    (3, 'p'): ['s3', 's2', 's1', 'c0', 'c1', 'c2', 'c3'],
+    (2, 'p'): ['s2', 's1', 'c0', 'c1', 'c2'],
+    (0, 'c'): ['1'],
+    (1, 'c'): ['x', 'y', 'z'],
+    (2, 'c'): ['xx', 'xy', 'xz', 'yy', 'yz', 'zz'],
+    (3, 'c'): ['xxx', 'xxy', 'xxz', 'xyy', 'xyz', 'xzz', 'yyy', 'yyz', 'yzz', 'zzz'],
+    (4, 'c'): ['xxxx', 'xxxy', 'xxxz', 'xxyy', 'xxyz', 'xxzz', 'xyyy', 'xyyz', 'xyzz',
+               'xzzz', 'yyyy', 'yyyz', 'yyzz', 'yzzz', 'zzzz'],
+    (5, 'c'): ['xxxxx', 'xxxxy', 'xxxxz', 'xxxyy', 'xxxyz', 'xxxzz', 'xxyyy', 'xxyyz', 'xxyzz', 'xxzzz', 'xyyyy', 'xyyyz', 'xyyzz', 'xyzzz',
+               'xzzzz', 'yyyyy', 'yyyyz', 'yyyzz', 'yyzzz', 'yzzzz', 'zzzzz'],
+    (6, 'c'): ['xxxxxx', 'xxxxxy', 'xxxxxz', 'xxxxyy', 'xxxxyz', 'xxxxzz', 'xxxyyy',
+               'xxxyyz', 'xxxyzz', 'xxxzzz', 'xxyyyy', 'xxyyyz', 'xxyyzz', 'xxyzzz',
+               'xxzzzz', 'xyyyyy', 'xyyyyz', 'xyyyzz', 'xyyzzz', 'xyzzzz', 'xzzzzz',
+               'yyyyyy', 'yyyyyz', 'yyyyzz', 'yyyzzz', 'yyzzzz', 'yzzzzz', 'zzzzzz'],
+}
+
 def from_iodata(mol, tol=1e-20, overlap=False):
+
     """Return basis set stored within the `IOData` instance in `iodata`.
 
     Parameters
@@ -112,6 +132,41 @@ def from_iodata(mol, tol=1e-20, overlap=False):
                 )
 
             return tuple(sph_conventions[self.angmom])
+
+        def permutation_libcint(self):
+            """Returns the permutation of the stored convention to the one used in libcint for
+            the given angular momentum
+
+            Conventions supported
+            cartesian: string of X, Y, and Z characters,such that :math:`a_x` `X` characters,
+            :math:`a_y` `Y` characters, and :math:`a_z` `Z`, characters appear. F
+            For example, `(2, 1, 1)` corresponds to `XXYZ`
+
+            Spherical: Strings of the form `c{m}` and `s{m}`.
+
+            Rerturns:
+            --------
+            permutation: An integer array that permutes basis function from IOData stored convention
+                         to LibCint.
+
+            """
+
+            angmom = self.angmom
+            if self.coord_type == "cartesian":
+                coord_type = 'c'
+                iodata_shell_convention = cart_conventions[angmom]
+            elif self.coord_type == 'spherical':
+                coord_type = 'p'
+                iodata_shell_convention = sph_conventions[angmom]
+
+            # Get libcint convention
+            print(angmom, coord_type)
+            libcint_convention = CONVENTIONS_LIBCINT[(angmom, coord_type)]
+            # Generate permutation
+            permutation = [libcint_convention.index(conv1) for conv1 in iodata_shell_convention]
+
+            return permutation
+
 
     if molbasis.primitive_normalization != "L2":  # pragma: no cover
         raise ValueError(
