140dffa494cfa0dcb8bc719df13f7e91f37ac053
ohzde <ohosseinzade0@gmail.com>
2025-07-13 22:29:51 -0700

    add `evaluate_basis_mask()` to `screening.py`

--- gbasis/screening.py
+++ gbasis/screening.py
@@ -1,6 +1,7 @@
 """1 and 2-index screening functions"""
 
 import numpy as np
+from scipy.special import factorial2, lambertw
 
 
 def is_two_index_overlap_screened(contractions_one, contractions_two, tol_screen):
@@ -24,7 +25,7 @@ def is_two_index_overlap_screened(contractions_one, contractions_two, tol_screen
     contractions_two : GeneralizedContractionShell
         Contracted Cartesian Gaussians (of the same shell) associated with the second index of
         the integral.
-    tol_screen : float, optional
+    tol_screen : float
         The tolerance used for screening two-index integrals. The `tol_screen` is combined with the
         minimum contraction exponents to compute a cutoff which is compared to the distance between
         the contraction centers to decide whether the integral should be set to zero.
@@ -42,3 +43,60 @@ def is_two_index_overlap_screened(contractions_one, contractions_two, tol_screen
     cutoff = np.sqrt(-(alpha_a + alpha_b) / (alpha_a * alpha_b) * np.log(tol_screen))
     # integrals are screened if centers are further apart than the cutoff
     return np.linalg.norm(r_12) > cutoff
+
+
+def evaluate_contraction_mask(contraction, points, tol_screen):
+    r"""Return a bolean mask of points that should be screened.
+
+    .. math::
+           d_{A_s;B_t} > \sqrt{-\frac{\alpha_{ min(\alpha_{s,A})} +
+           \alpha_{ min(\alpha_{t,B})} }{ \alpha_{ min(\alpha_{s,A})}
+            \alpha_{ min(\alpha_{t,B})} } \ln \varepsilon }
+
+    where :math:`d` is the cut-off distance at which shells do not interact with each other.
+    :math:`A` and `B` are the atoms each contraction are respectively centered on.
+    :math: `\alpha` is the gaussian exponent
+    :math: `s` and `t` index primitive Gaussian shells centered on atom `A` and `B` respectively.
+
+    Parameters
+    ----------
+    contraction : GeneralizedContractionShell
+        Contracted Cartesian Gaussians (of the same shell) associated with the first index of
+        the integral.
+    points : np.ndarray(N, 3)
+        Cartesian coordinates of the points in space (in atomic units) where the basis
+        functions are evaluated.
+        Rows correspond to the points and columns correspond to the :math:`x, y, \text{and} z`
+        components.
+    tol_screen : float
+        The tolerance used for screening a contraction at grid points. `tol_screen` is combined
+        with the minimum contraction parameters to compute a cutoff distance. This cutoff is
+        compared against all grid points, point farther than the cutoff will be excluded
+        from evaluation of the contraction.
+
+    Returns
+    -------
+    array : `bool` (N, 3)
+        For each grid point, if evaluation should be screened, return `False`
+    """
+
+    # minimum contraction parameters
+    cmin = np.abs(contraction.coeffs.min())
+    amin = contraction.exps.min()
+    angm = contraction.angmom
+    nmin = (
+        (2 * amin / np.pi) ** (3 / 4)
+        * ((4 * amin) ** (angm / 2))
+        / np.sqrt(factorial2(2 * angm + 1))
+    )
+
+    # log formula for l = 0
+    if angm == 0:
+        dist2 = -np.log(tol_screen / (cmin * nmin)) / amin
+    # lambert formula otheriwse
+    else:
+        w = lambertw((tol_screen / (cmin * nmin)), k=-1)
+        x = -w.real / amin
+        dist2 = x
+
+    return np.sum((points - contraction.coord) ** 2, axis=1) <= dist2
