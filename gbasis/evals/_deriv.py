f58b7083a51de5878141e7ec3c28bc141c1de17a
Aayush Gupta <aayushgupta@Aayushs-MacBook-Air.local>
2026-08-12 08:08:32 +0530

    fix: use .real cast for eval_hermite Windows compat

--- gbasis/evals/_deriv.py
+++ gbasis/evals/_deriv.py
@@ -129,9 +129,9 @@ def _eval_deriv_contractions(coords, orders, center, angmom_comps, alphas, prim_
         # to evaluate multiple orders at the same time. Creating/finding a better function for
         # evaluating the hermite polynomial at different orders (in sequence) may be nice in the
         # future.
-        coeffs = coeffs.astype(np.float64)
+        coeffs = coeffs.real.astype(np.float64)
         hermite = np.sum(
-            coeffs * eval_hermite(indices_herm.astype(int), (alphas**0.5 * nonzero_coords).astype(np.float64)), axis=0
+            coeffs * eval_hermite(indices_herm.astype(int), (alphas**0.5 * nonzero_coords).real.astype(np.float64)), axis=0
         )
         hermite = np.prod(hermite, axis=1)
 
